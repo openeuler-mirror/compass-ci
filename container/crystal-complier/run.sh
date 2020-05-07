8f7cd4c51742e1fd85877defd2bb1ece601b6974
Cao xueliang <caoxueliang2@huawei.com>
2020-05-07 14:39:28 +0800

    add git error branch

--- container/crystal-complier/run.sh
+++ container/crystal-complier/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-pj_dir=`git rev-parse --show-toplevel`
-echo $pj_dir
+pj_dir=`git rev-parse --show-toplevel 2>/dev/null`
+if [ $pj_dir ];then
+    v_dir=$pj_dir
+else
+    v_dir=$PWD
+fi
 cmd=(
 	docker run
 	-u $UID 
 	--rm
 	-it
-	-v $pj_dir:$pj_dir
+	-v $v_dir:$v_dir
 	-w $PWD 	 
 	alpine:crystal-complier
 	crystal build --static "$@"
