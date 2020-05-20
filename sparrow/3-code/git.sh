2b88ebbe08e1adb8daf3e19f98171b6a18d74adf
Wu Fengguang <wufengguang@huawei.com>
2020-05-21 07:25:55 +0800

    sparrow: add git clone
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

---
+++ sparrow/3-code/git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# download git trees
+
+mkdir /c
+cd /c
+
+# git clone https://gitee.com/openeuler/crystal-ci.git
+# modify and manual run for now:
+git clone ssh://crystal/c/crystal-ci.git
+
+git clone https://gitee.com/wu_fengguang/lkp-tests.git
