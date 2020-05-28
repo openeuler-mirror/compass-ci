9e0b7f43c5c0bce8d97abea0adc15fe1e2f4d66e
Cao xueliang <caoxueliang2@huawei.com>
2020-05-28 19:10:08 +0800

    scheduler dynamic support the mapping relationship between job yaml and os dir

--- scheduler/src/scheduler/boot.cr
+++ scheduler/src/scheduler/boot.cr
@@ -16,27 +16,31 @@ require  "./resources"
 module Scheduler
     module Boot
 	def self.ipxe_msg(msg)
-	    "#!ipxe\n\n
-	    echo ...\n
-	    echo #{msg}\n
-	    echo ...\n
+	    "#!ipxe
+	    echo ...
+	    echo #{msg}
+	    echo ...
 	    reboot"
 	end
 
         def self.respon(job_content : JSON::Any, env, resources : Scheduler::Resources) 
-            respon = "#!ipxe\n\n"
+	    if job_content["os"]?
+		os_dir = job_content["os"].to_s + "/" + job_content["os_arch"].to_s + "/" + job_content["os_version"].to_s
+	    else
+		os_dir = "debian/aarch64/sid"
+	    end
 
             # the localhost should configure to <os and lkp> hostname
             server = "172.168.131.113"
             scheduler = "172.17.0.1"
 
-            # job_id = "29" # temp set to fix job (corresponding to job config)
-            respon = respon + "initrd http://#{server}:8000/os/debian/aarch64/sid/initrd.lkp\n"
+            respon = "#!ipxe\n\n"
+            respon = respon + "initrd http://#{server}:8000/os/#{os_dir}/initrd.lkp\n"
             respon = respon + "initrd http://#{server}:8800/initrd/lkp/latest/lkp-aarch64.cgz\n"
             respon = respon + "initrd http://#{scheduler}:3000/job_initrd_tmpfs/#{job_content["id"]}/job.cgz\n"
-            respon = respon + "kernel http://#{server}:8000/os/debian/aarch64/sid/vmlinuz user=lkp"
+            respon = respon + "kernel http://#{server}:8000/os/#{os_dir}/vmlinuz user=lkp"
             respon = respon + " job=/lkp/scheduled/job.yaml RESULT_ROOT=/result/job"
-            respon = respon + " root=#{server}:/os/debian/aarch64/sid rootovl ip=enp0s1:dhcp ro"
+            respon = respon + " root=#{server}:/os/#{os_dir} rootovl ip=enp0s1:dhcp ro"
             respon = respon + " initrd=initrd.lkp initrd=lkp-aarch64.cgz initrd=job.cgz\n"
             respon = respon + "boot\n"
             return respon
