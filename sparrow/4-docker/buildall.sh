9aa273333d37882e0b9f829a5cf27765f6bb2cd5
Liu Yinsi <liuyinsi@huawei.com>
2020-06-01 17:50:06 +0800

    modify 4-docker/buildall.sh

--- sparrow/4-docker/buildall.sh
+++ sparrow/4-docker/buildall.sh
@@ -1,12 +1,27 @@
 #!/bin/bash -e
-
 [[ $CCI_SRC ]] || export CCI_SRC=$(dirname $(dirname $(dirname $(realpath $0))))
 CONTAINER_PATH="$CCI_SRC/container"
 
 for dir in $CONTAINER_PATH/*
 do
+	[ "${dir##*/}" == 'scheduler' ] && {
+		echo "${dir##*/} not ready to build&run, skip!"
+		continue
+	}
 	cd "$dir"
 	./build.sh
+	[ "${dir##*/}" == 'debian' ] || \
+	[ "${dir##*/}" == 'lkp-initrd' ] || \
+	[ "${dir##*/}" == 'dracut-initrd' ] || \
+	[ "${dir##*/}" == 'crystal-base' ] || \
+	[ "${dir##*/}" == 'scheduler-dev' ] && {
+		echo "${dir##*/} just build, skip!"
+		continue
+	}
+	[ "${dir##*/}" == 'es' ] && {
+		echo "${dir##*/} not ready to run, skip!"
+		continue
+	}
 	[ "${dir##*/}" == 'crystal-compiler' ] && {
 		./install.sh
 		continue
