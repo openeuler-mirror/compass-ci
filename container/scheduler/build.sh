f9d10a1e299f1825029e1e6d967bf1457f8200ce
sunlijun <sunlijun5@huawei.com>
2020-06-01 17:40:49 +0800

    fix scheduler build images

--- container/scheduler/build.sh
+++ container/scheduler/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+bash ../../scheduler/build.sh
+cp ../../scheduler/scheduler .
+cp /c/lkp-tests/sbin/create-job-cpio.sh .
+
 docker build -t sch-ruby-a:v0.00d .
 
+rm -f scheduler
+rm -f ../../scheduler/scheduler
+rm -f create-job-cpio.sh
