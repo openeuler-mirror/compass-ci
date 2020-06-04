53786d7a6554cbd3a770843ddcb33fd51bba489e
Wu Fengguang <wufengguang@huawei.com>
2020-06-04 21:22:42 +0800

    per-user lkp initrd
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

--- container/lkp-initrd/run.sh
+++ container/lkp-initrd/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+[[ $CCI_LAB ]] || CCI_LAB=sparrow
 [[ $LKP_SRC ]] || LKP_SRC=/c/lkp-tests
-DIR=$(dirname $(realpath $0))
 
+. $LKP_SRC/lib/yaml.sh
+create_yaml_variables "$LKP_SRC/labs/${CCI_LAB}.yaml"
+
+DIR=$(dirname $(realpath $0))
 cmd=(
 	docker run
 	--rm
@@ -10,7 +14,7 @@ cmd=(
 	-e LKP_SRC=$LKP_SRC
 	-v $LKP_SRC:$LKP_SRC
 	-v $DIR/bin:/root/bin
-	-v /srv/initrd/lkp/latest:/osimage/user/lkp
+	-v /srv/initrd/lkp/${lkp_initrd_user:-latest}:/osimage/user/lkp
 	alpine:lkp
 	/root/bin/pack-lkp.sh
 )
