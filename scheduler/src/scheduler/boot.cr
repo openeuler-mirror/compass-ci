acaf8b8a58bd9c934310743a187b950398e60ba4
WuZhende <wuzhende1@huawei.com>
2020-06-04 19:57:49 +0800

    lkp_initrd_user
    get lkp_initrd_user from job.yaml
    Signed-off-by: Wu Zhende <wuzhende1@huawei.com>

--- scheduler/src/scheduler/boot.cr
+++ scheduler/src/scheduler/boot.cr
@@ -24,11 +24,16 @@ module Scheduler
 	end
 
         def self.respon(job_content : JSON::Any, env, resources : Scheduler::Resources) 
-	    if job_content["os"]?
-		os_dir = job_content["os"].to_s + "/" + job_content["os_arch"].to_s + "/" + job_content["os_version"].to_s
-	    else
-		os_dir = "debian/aarch64/sid"
-	    end
+            if job_content["os"]?
+                os_dir = job_content["os"].to_s + "/" + job_content["os_arch"].to_s + "/" + job_content["os_version"].to_s
+            else
+                os_dir = "debian/aarch64/sid"
+            end
+            if job_content["lkp_initrd_user"]?
+                lkp_initrd_user = job_content["lkp_initrd_user"].to_s
+            else
+                lkp_initrd_user = "latest"
+            end
 
             # the localhost should configure to <os and lkp> hostname
             server = "172.168.131.113"
@@ -36,7 +41,7 @@ module Scheduler
 
             respon = "#!ipxe\n\n"
             respon = respon + "initrd http://#{server}:8000/os/#{os_dir}/initrd.lkp\n"
-            respon = respon + "initrd http://#{server}:8800/initrd/lkp/latest/lkp-aarch64.cgz\n"
+            respon = respon + "initrd http://#{server}:8800/initrd/lkp/#{lkp_initrd_user}/lkp-aarch64.cgz\n"
             respon = respon + "initrd http://#{scheduler}:3000/job_initrd_tmpfs/#{job_content["id"]}/job.cgz\n"
             respon = respon + "kernel http://#{server}:8000/os/#{os_dir}/vmlinuz user=lkp"
             respon = respon + " job=/lkp/scheduled/job.yaml RESULT_ROOT=/result/job"
