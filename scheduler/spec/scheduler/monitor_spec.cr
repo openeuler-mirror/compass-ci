38dd602020d24ef6f40abb3039e302e9d99b8187
chief <tongqunfeng@huawei.com>
2020-06-06 02:34:36 +0800

    simplify moniter_spec
    qemu demo passed job_id = 1341
    and all crystal spec passed (27 examples)

--- scheduler/spec/scheduler/monitor_spec.cr
+++ scheduler/spec/scheduler/monitor_spec.cr
@@ -15,7 +15,7 @@ end
 
 describe Scheduler::Monitor do
     describe "job maintain" do
-        it "recieve job parameters, then update the job parametre" do
+        it "recieve job parameters, then update the job parameter in redis" do
             context = gen_put_context("/~lkp/cgi-bin/lkp-jobfile-append-var?job_file=/lkp/scheduled/job.yaml&job_id=100&loadavg=0.28 0.82 0.49 1/105 3389&start_time=1587725398&end_time=1587725698")
             parameter_key = "start_time"
 
@@ -24,24 +24,18 @@ describe Scheduler::Monitor do
             parameter_value =  context.request.query_params[parameter_key]
 
             resources = Scheduler::Resources.new
-            resources.redis_client(JOB_REDIS_HOST,JOB_REDIS_PORT_DEBUG)
+            resources.redis_client(JOB_REDIS_HOST, JOB_REDIS_PORT_DEBUG)
+            raw_redis = Redis.new(JOB_REDIS_HOST, JOB_REDIS_PORT_DEBUG)
+            raw_redis.del("sched/id2job")
 
             # add 100, this job contains { testbox: wfg-e595, tbox_group: wfg-e595}
-            resources.fsdir_root("/usr/share/code/scheduler/public")
+            resources.fsdir_root("./public")
 
-            hash = { "id" => job_id, parameter_key => parameter_value }
-            Scheduler::Monitor.update_job_parameter(hash, context, resources)
+            job_content = { "id" => job_id, parameter_key => parameter_value }
+            Scheduler::Monitor.update_job_parameter(job_content, context, resources)
 
-            respon = resources.@redis_client.not_nil!.@client.hget("sched/id2job", job_id).not_nil!
-
-            case respon
-            when JSON::Any
-                respon_hash = respon.as_h
-                
-            else String
-                respon_hash = JSON.parse(respon).as_h
-            end
-            (respon_hash[parameter_key]).should eq("1587725398")
+            response = resources.@redis_client.not_nil!.get_job_content(job_id)
+            (response[parameter_key]).should eq("1587725398")
         end
     end
 end
