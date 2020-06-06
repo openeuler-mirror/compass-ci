7f64644c6ba8ee9ff3a34cdc07ff29e556006c7b
Wu Fengguang <wufengguang@huawei.com>
2020-06-06 19:48:59 +0800

    sparrow: export default CCI_LAB
    Tong Qunfeng wrote:
    >you can find:
    >
    >curl http://172.17.0.1:3000
    >curl http://192.168.177.1:3000
    >curl http://172.168.131.113:3000
    >
    >this 3 curl command will return: "LKP Alive! ..."
    
    The 3 networks are
    
    br0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
            inet 192.168.177.1  netmask 255.255.255.0  broadcast 0.0.0.0
    
    enp125s0f0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
            inet 172.168.131.113  netmask 255.255.255.0  broadcast 172.168.131.255
            inet6 fe80::6ed5:faef:7007:eaf7  prefixlen 64  scopeid 0x20<link>
            ether b0:08:75:0b:2f:df  txqueuelen 1000  (Ethernet)
    
    docker0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
            inet 172.17.0.1  netmask 255.255.0.0  broadcast 172.17.255.255
    
    wfg@crystal ~/crystal-ci% route
    Kernel IP routing table
    Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
    0.0.0.0         172.168.131.3   0.0.0.0         UG    100    0        0 enp125s0f0
    172.17.0.0      0.0.0.0         255.255.0.0     U     0      0        0 docker0
    172.168.131.0   0.0.0.0         255.255.255.0   U     100    0        0 enp125s0f0
    192.168.122.0   0.0.0.0         255.255.255.0   U     0      0        0 virbr0
    192.168.177.0   0.0.0.0         255.255.255.0   U     0      0        0 br0
    
    I started kvm-debian.sh and find an interesting dilemma.
    
    root@debian:~# curl http://172.17.0.1:3000
    curl: (7) Failed to connect to 172.17.0.1 port 3000: Connection refused
    
    The root cause is it has a local docker0 network.
    So intercepted the request. docker0 address looks not reliable.
    
    HOST IP has best (inside and outside) connectivity, however need
    auto detect for each deployment.
    
    The br0 address can be hard coded, however only works if it's a local
    deployment and the host is a gateway, otherwise the internal physical
    machines cannot route to it.
    
    root@debian:~# route
    Kernel IP routing table
    Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
    default         192.168.177.1   0.0.0.0         UG    0      0        0 enp0s1
    172.17.0.0      0.0.0.0         255.255.0.0     U     0      0        0 docker0
    192.168.177.0   0.0.0.0         255.255.255.0   U     0      0        0 enp0s1
    
    The other 2 curl commands will succeed.
    
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

--- sparrow/3-code/dev-env.sh
+++ sparrow/3-code/dev-env.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cat > /etc/profile.d/crystal.sh <<EOF
-export LKP_SRC=/c/lkp-tests
-export CCI_SRC=/c/crystal-ci
-EOF
-
 server_name=$(hostname | cut -f1 -d.)
 server_ip=$(ip route get 1.2.3.4 | awk '{print $7; exit}')
 cat > /c/lkp-tests/labs/$server_name.yaml <<EOF
@@ -12,4 +7,10 @@ SCHED_IP: $server_ip
 SCHED_PORT: 3000
 LKP_SERVER: $server_ip
 LKP_CGI_PORT: 3000
+EOF
+
+cat > /etc/profile.d/crystal.sh <<EOF
+export LKP_SRC=/c/lkp-tests
+export CCI_SRC=/c/crystal-ci
+export CCI_LAB=$server_name
 EOF
