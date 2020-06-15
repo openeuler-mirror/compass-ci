6bdf70b903fdc02e047caa819a2685914a9d9196
Wu Fengguang <wufengguang@huawei.com>
2020-06-15 13:15:03 +0800

    container: mount setup.sh instead of COPY
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

--- container/alpine/first-run.sh
+++ container/alpine/first-run.sh
@@ -6,6 +6,7 @@ cmd=(
 	-v alpine-home:/home
 	-v alpine-root:/root
 	-v /etc/skel:/mnt/skel
+	-v $CCI_SRC/container/setup.sh:/usr/local/sbin/setup.sh
 	-e SSH_KEYS="$(</etc/ssh/team.pub)"
 	alpine:testbed
 	bash
