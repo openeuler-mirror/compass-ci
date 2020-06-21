fed6c4471e2c89ff8124d8b55902de746ce3537a
Wu Fengguang <wufengguang@huawei.com>
2020-06-21 21:31:25 +0800

    sparrow: create system defaults instead of CCI_LAB
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

--- sparrow/3-code/dev-env.sh
+++ sparrow/3-code/dev-env.sh
@@ -2,7 +2,9 @@
 
 server_name=$(hostname | cut -f1 -d.)
 server_ip=$(ip route get 1.2.3.4 | awk '{print $7; exit}')
-cat > /c/lkp-tests/labs/$server_name.yaml <<EOF
+
+mkdir -p /etc/crystal-ci/defaults
+cat >    /etc/crystal-ci/defaults/$server_name.yaml <<EOF
 SCHED_IP: $server_ip
 SCHED_PORT: 3000
 LKP_SERVER: $server_ip
@@ -12,7 +14,6 @@ EOF
 cat > /etc/profile.d/crystal.sh <<EOF
 export LKP_SRC=/c/lkp-tests
 export CCI_SRC=/c/crystal-ci
-export CCI_LAB=$server_name
 
 PATH="$PATH:$CCI_SRC/sbin"
 PATH="$PATH:$LKP_SRC/sbin:$LKP_SRC/bin"
