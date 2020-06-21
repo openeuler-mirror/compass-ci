7aae4943a530a2559ad69eed29924e47672ae527
Wu Fengguang <wufengguang@huawei.com>
2020-06-21 20:47:45 +0800

    container: add lab.rb
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

---
+++ container/lab.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'yaml'
+
+def cci_defaults
+  hash = {}
+  Dir.glob(['/etc/crystal-ci/defaults/*.yaml',
+            "#{ENV['HOME']}/.config/crystal-ci/defaults/*.yaml"]).each do |file|
+    hash.update YAML.load_file(file)
+  end
+  hash
+end
+
+def relevant_defaults(names)
+  cci_defaults.select { |k, _| names.include? k }
+end
+
+def docker_env(hash)
+  hash.map { |k, v| ['-e', "#{k}=#{v}"] }.flatten
+end
