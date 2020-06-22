d9fb251d0e1a6dc10b481a6add63ee55c9eb94be
Sun Lijun <sunlijun5@huawei.com>
2020-06-22 16:30:24 +0800

    scheduler: add assign key<id,suite,os,os_version> to controllable
    Signed-off-by: Sun Lijun <sunlijun5@huawei.com>

--- src/lib/job.cr
+++ src/lib/job.cr
@@ -4,13 +4,18 @@ require "yaml"
 class Job
 
     @job_hash : Hash(String, JSON::Any)
+    ASSIGN_KEY = %w(id suite os os_version)
 
     def initialize(job_content : JSON::Any)
         @job_hash = job_content.as_h
     end
 
     macro method_missing(call)
-        @job_hash[{{ call.name.stringify }}].to_s
+        if ASSIGN_KEY.includes?({{ call.name.stringify }})
+            @job_hash[{{ call.name.stringify }}].to_s
+        else
+            raise "Unassigned key"
+        end
     end
 
     def dump_to_json()
