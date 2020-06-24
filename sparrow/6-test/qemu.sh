5f626de650d664679cb78c94b63d832303850658
Liu Yinsi <liuyinsi@huawei.com>
2020-06-24 10:34:34 +0800

    deploy: run job test using pxe or kvm

--- sparrow/6-test/qemu.sh
+++ sparrow/6-test/qemu.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -e
 
-## TODO
-# 1.submit a job
-# 2.run qemu.sh
+cd /c/cci/user-client/helper
+
+run_job()
+{
+	./0_addjob.sh ../jobs/$1
+	./2_runqemu.sh
+}
+
+export hostname=vm-pxe-hi1620-2p8g-1
+run_job iperf-pxe.yaml
+
+export hostname=vm-hi1620-2p8g-1
+run_job iperf-vm.yaml
+
+
+
+
+
+
+
