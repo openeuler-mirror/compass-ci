f36fac575d65fab832f63d1fe2913eef02eb48a1
Tong Qunfeng <tongqunfeng@huawei.com>
2020-06-28 18:27:33 +0800

    API: hand_over for TaskQueue
    1) add support to HAND_OVER API
    2) refactor queue_check_params to support multi-params
    3) tiny fix to some comment
    
    Signed-off-by: Tong Qunfeng <tongqunfeng@huawei.com>

--- src/taskqueue/queue.cr
+++ src/taskqueue/queue.cr
@@ -8,10 +8,10 @@ class TaskQueue
       return queue_respond_header_set(env, 400, "Missing http body")
     end
 
-    queue_name, ext_set = queue_check_params(env, "queue")
+    queue_name, ext_set = queue_check_params(env, ["queue"])
     return ext_set if queue_name.nil?
 
-    queue_name = queue_name + "/ready"
+    queue_name = queue_name[0] + "/ready"
 
     task_content = JSON.parse(body.gets_to_end)
     id = task_content["id"]?
@@ -32,22 +32,27 @@ class TaskQueue
     env.response.headers.add("CCI-Error-Description", message)
   end
 
-  def queue_check_params(env, parameter_name)
-    parameter_value = env.params.query[parameter_name]?
+  def queue_check_params(env, parameter_list)
+    params = Array(String).new
     ext_set = nil
-    if parameter_value.nil?
-       ext_set = queue_respond_header_set(env, 400, "Missing parameter <#{parameter_name}>")
+    parameter_list.each do |parameter_name|
+      parameter_value = env.params.query[parameter_name]?
+      if parameter_value.nil?
+         ext_set = queue_respond_header_set(env, 400, "Missing parameter <#{parameter_name}>")
+         return params, ext_set
+      end
+      params << parameter_value
     end
 
-    return parameter_value, ext_set
+    return params, ext_set
   end
 
   def queue_respond_consume(env)
-    queue_name, ext_set = queue_check_params(env, "queue")
+    queue_name, ext_set = queue_check_params(env, ["queue"])
     return ext_set if queue_name.nil?
 
-    queue_name_from = queue_name + "/ready"
-    queue_name_to   = queue_name + "/in_process"
+    queue_name_from = queue_name[0] + "/ready"
+    queue_name_to   = queue_name[0] + "/in_process"
 
     if id = find_first_task_in_redis(queue_name_from)
       env.response.status_code = 200
@@ -58,5 +63,22 @@ class TaskQueue
     end
 
     return task
+  end
+
+  def queue_respond_hand_over(env)
+    params, ext_set = queue_check_params(env, ["from", "to", "id"])
+    return ext_set if ext_set != nil
+
+    from = params[0] + "/in_process"
+    to   = params[1] + "/ready"
+    id   = params[2]
+
+    if move_task_in_redis(from, to, id)
+      env.response.status_code = 201
+    else
+      queue_respond_header_set(env, 409, "Can not find id <#{id}> in queue <#{params[0]}>")
+    end
+
+    return nil
   end
 end
--- src/taskqueue/taskqueue.cr
+++ src/taskqueue/taskqueue.cr
@@ -29,10 +29,10 @@ class TaskQueue
     # request: curl -X POST http://localhost:3060/add?queue=scheduler/$tbox_group
     #               -H "Content-Type: application/json"
     #               --data '{"suite":"test01", "tbox_group":"host"}'
-    #          |    --data '{"suite":"test01", "id":1, "tbox_group":"host"}'
+    #          |    --data '{"suite":"test01", "id":$id, "tbox_group":"host"}'
     #
     # response: 200 {id: 1}.to_json
-    #           409 "Queue <scheduler/host> already has id <1>"
+    #           409 "Queue <scheduler/host> already has id <$id>"
     #           409 "Add with error id <65536>"
     #           400 "Missing parameter <queue>"
     #           400 "Missing http body"
@@ -56,6 +56,18 @@ class TaskQueue
       if env.response.status_code == 200
         response
       end
+    end
+
+    # -------------------
+    # request: curl -X PUT http://localhost:3060/hand_over?
+    #          from=scheduler/$tbox_group&to=extract_stats&id=$id
+    #
+    # response: 201 ## when succeed hand over
+    #           400 "Missing parameter <from|to|id>"
+    #           409 "Can not find id <$id> in queue <scheduler/$tbox_group>"
+    put "/hand_over" do |env|
+      response = queue_respond_hand_over(env)
+      debug_message(env, response)
     end
 
     @port = (ENV.has_key?("TASKQUEUE_PORT") ? ENV["TASKQUEUE_PORT"].to_i32 : TASKQUEUE_PORT)
