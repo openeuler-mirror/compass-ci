a11416d958a26d2bf9c8973b0376f51241d5c77b
Wu Fengguang <wufengguang@huawei.com>
2020-06-29 08:04:07 +0800

    multi-qemu-mac.rb: fix rubocop warnings
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

--- providers/multi-qemu-mac.rb
+++ providers/multi-qemu-mac.rb
@@ -1,40 +1,39 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Run multiple QEMU in parallel
 class MultiQEMU
-   attr_accessor :hostname
-   attr_accessor :nr_vm
-  def initialize(hostname="vm-pxe-hi1620-1p1g", nr_vm=2)
-    @hostname=hostname
-    @nr_vm=nr_vm
+  attr_accessor :hostname
+  attr_accessor :nr_vm
+  def initialize(hostname = 'vm-pxe-hi1620-1p1g', nr_vm = 2)
+    @hostname = hostname
+    @nr_vm = nr_vm
   end
 
-  def run(i)
+  def run(seqno)
     loop do
       hostname = self.hostname.strip
-      res=system({"hostname"=>(hostname + "-" + i.to_s)}, ENV["CCI_SRC"]+ "/providers/qemu.sh")
-      if res
-        next
-      else
-        puts "Error..."
+      ret = system({ 'hostname' => (hostname + '-' + seqno.to_s) }, ENV['CCI_SRC'] + '/providers/qemu.sh')
+      unless ret
+        puts 'Error...'
         break
       end
     end
   end
 
-  def start  
-    puts "keep the main process started, ctrl + c quit ALL"
-    self.nr_vm.times { |i|
-      Thread.new{(self.run(i))}
-    }
-    #keep the main process started, ctrl + c quit ALL
-    sleep 60*60*12
+  def start
+    puts 'keep the main process started, ctrl + c quit ALL'
+    nr_vm.times do |i|
+      Thread.new { run(i) }
+    end
+    # keep the main process started, ctrl + c quit ALL
+    sleep 60 * 60 * 12
   end
 end
 
-if $PROGRAM_NAME == __FILE__ 
+if $PROGRAM_NAME == __FILE__
   t = MultiQEMU.new
-  t.hostname="vm-pxe-hi1620-2p4g"
+  t.hostname = 'vm-pxe-hi1620-2p4g'
   t.nr_vm = 2
   t.start
-
 end
-
-
