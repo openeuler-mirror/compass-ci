9cfebe0ddf58a6aaeceaa8a2fc0af6c158af8ea1
Yu Chuan <yuchuan6@huawei.com>
2020-06-30 10:50:31 +0800

    dracut-initrd: Modify apt original source repo to 163 source repo
    sources.list:
    - the sources.list that skips certificate verification;
    - used to install ca-certificates only;
    
    sources.list.bak:
    - the sources.list that does not skips certificate verification;
    - used to install other dependent packages as needed;
    
    Dockerfile:
    - Add the steps to copy two sources.list.
    
    set-dracut.sh:
    - Use source.list to install the ca-certificates;
    - Then "mv source.list.bak source.list" to provide a safe, fast
      way to install subsequent dependency packages.

--- container/dracut-initrd/bin/setup-dracut.sh
+++ container/dracut-initrd/bin/setup-dracut.sh
@@ -2,6 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# change apt source repo to 163
+apt-get update
+apt-get install -y ca-certificates
+mv /etc/apt/sources.list.bak /etc/apt/sources.list
+
 apt-get update
 apt-get install -y apt-utils nfs-common netbase
 apt-get install -y dracut dracut-network dracut-config-generic
