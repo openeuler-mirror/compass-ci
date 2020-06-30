cca1bbb6dba6737ca86649aaaf2e1bee864a81ac
Li Yuanchao <liyuanchao7@huawei.com>
2020-06-30 16:55:44 +0800

    introduce git mirror service
    input: $LKP_SRC/repo/$project/$fork
    output: /srv/git/$project/$fork.git
    
    The main & worker threads follow producer-consumer model.
    
    Signed-off-by: Li Yuanchao <liyuanchao7@huawei.com>
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

---
+++ lib/git_mirror.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'yaml'
+require 'fileutils'
+
+# worker threads
+class GitMirror
+  def initialize(queue)
+    @queue = queue
+  end
+
+  def mirror_once
+    fork_info = @queue.pop
+    mirror_dir = '/srv/git/' + fork_info['forkdir'] + '.git'
+    if File.directory?(mirror_dir)
+      system("git -C #{mirror_dir} fetch")
+    else
+      FileUtils.mkdir_p(mirror_dir)
+      system("git clone --bare #{fork_info['url']} #{mirror_dir}")
+    end
+  end
+
+  def gitmirror
+    loop do
+      mirror_once
+    end
+  end
+end
+
+# main thread
+class MirrorMain
+  def initialize
+    @git_info = {}
+    @git_queue = SizedQueue.new(10)
+    load_fork_info
+  end
+
+  attr_reader :git_info
+  attr_reader :git_queue
+
+  def load_fork_info
+    repodir = "#{ENV['LKP_SRC']}/repo"
+    project_list = Dir.entries(repodir) - Array['.', '..']
+    project_list.each do |project|
+      project_dir = "#{repodir}/#{project}"
+      fork_list = Dir.entries(project_dir) - Array['.', '..', 'DEFAULTS', '.ignore']
+      fork_list.each do |fork_name|
+        @git_info["#{project}/#{fork_name}"] = YAML.safe_load(File.open("#{project_dir}/#{fork_name}"))
+        @git_info["#{project}/#{fork_name}"]['forkdir'] = "#{project}/#{fork_name}"
+      end
+    end
+  end
+
+  def create_workers
+    10.times do
+      Thread.new do
+        git_mirror = GitMirror.new(@git_queue)
+        git_mirror.gitmirror
+      end
+      sleep(0.1)
+    end
+  end
+
+  def fork_loop
+    loop do
+      @git_info.each do |_key, value|
+        fork_info = value
+        @git_queue.push(fork_info)
+        sleep(10)
+      end
+    end
+  end
+end
---
+++ sbin/git-mirror.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "#{ENV['CCI_SRC']}/lib/git_mirror"
+
+git_mirror = MirrorMain.new
+
+git_mirror.create_workers
+git_mirror.fork_loop
