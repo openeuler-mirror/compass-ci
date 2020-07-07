bffdb1a3137bb908d22de35d7fe918ecd6468d04
Sun Lijun <sunlijun5@huawei.com>
2020-07-07 14:03:47 +0800

    style: job: fix white spaces
    Signed-off-by: Sun Lijun <sunlijun5@huawei.com>

--- src/lib/job.cr
+++ src/lib/job.cr
@@ -19,23 +19,23 @@ class Job
     ASSIGN_KEY = %w(id suite os os_version)
 
     def initialize(job_content : JSON::Any)
-        @job_hash = job_content.as_h
+      @job_hash = job_content.as_h
     end
 
     macro method_missing(call)
-        if ASSIGN_KEY.includes?({{ call.name.stringify }})
-            @job_hash[{{ call.name.stringify }}].to_s
-        else
-            raise "Unassigned key"
-        end
+      if ASSIGN_KEY.includes?({{ call.name.stringify }})
+        @job_hash[{{ call.name.stringify }}].to_s
+      else
+        raise "Unassigned key"
+      end
     end
 
     def dump_to_json()
-        @job_hash.to_json
+      @job_hash.to_json
     end
 
     def dump_to_yaml()
-        @job_hash.to_yaml
+      @job_hash.to_yaml
     end
 
     def update(hash : Hash)
