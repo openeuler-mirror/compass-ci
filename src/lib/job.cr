f4e9c4b0ae5b649ff7274d1c60ed07b1d53fc891
Sun Lijun <sunlijun5@huawei.com>
2020-07-08 15:37:46 +0800

    scheduler: job: set default values
    use any_merge to assign job_hash
    
    Signed-off-by: Sun Lijun <sunlijun5@huawei.com>

--- src/lib/job.cr
+++ src/lib/job.cr
@@ -16,10 +16,21 @@ end
 class Job
 
     @job_hash : Hash(String, JSON::Any)
-    ASSIGN_KEY = %w(id suite os os_version)
+    ASSIGN_KEY = %w(
+      id lkp_initrd_user os os_arch
+      os_dir os_version result_root
+      suite tbox_group
+    )
+    INIT_FIELD = {
+      os: "debian",
+      os_arch: "aarch",
+      os_version: "sid",
+      lkp_initrd_user: "latest"
+    }
 
     def initialize(job_content : JSON::Any)
       @job_hash = job_content.as_h
+      set_defaults()
     end
 
     macro method_missing(call)
@@ -45,4 +56,40 @@ class Job
     def update(json : JSON::Any)
       @job_hash.any_merge!(json.as_h)
     end
+
+    private def set_defaults()
+      append_init_field()
+      set_os_dir()
+      set_result_root()
+      set_tbox_group()
+    end
+
+    private def append_init_field()
+      INIT_FIELD.each do |k, v|
+        k = k.to_s
+        if !@job_hash[k]? || @job_hash[k] == nil
+          @job_hash.any_merge!({k => v})
+        end
+      end
+    end
+
+    private def set_os_dir()
+      @job_hash.any_merge!({"os_dir" => "#{os}/#{os_arch}/#{os_version}"})
+    end
+
+    private def set_result_root()
+      @job_hash.any_merge!({"result_root" => "/result/#{suite}/#{id}"})
+    end
+
+    private def set_tbox_group()
+      if !@job_hash["tbox_group"]?
+        find = @job_hash["testbox"].to_s.match(/(.*)(\-\d{1,}$)/)
+        if find != nil
+          @job_hash.any_merge!({"tbox_group" => find.not_nil![1]})
+        else
+          @job_hash.any_merge!({"tbox_group" => @job_hash["testbox"]})
+        end
+      end
+    end
+
 end
