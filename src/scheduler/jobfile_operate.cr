650c47b0137b4da603b1216ea9bc9b2628154b87
Cao Xueliang <caoxueliang2@huawei.com>
2020-07-13 10:22:32 +0800

    sched: delete useless code
    Delete useless code in jobfile_operate.cr.
    
    Signed-off-by: Cao Xueliang <caoxueliang2@huawei.com>

--- src/scheduler/jobfile_operate.cr
+++ src/scheduler/jobfile_operate.cr
@@ -12,42 +12,6 @@ end
 
 module Jobfile::Operate
 
-    def self.update(file_path : String, section : String, kv : Hash)
-        section_find = -1
-        kv_find = false
-        kv_key = kv.first_key
-
-        job_array = File.read_lines(file_path)
-        job_array.each_index do |index|
-            line = job_array[index]
-            match_info = line.match(/#{kv_key}: (.*)/)
-            if match_info
-                line = line.sub("#{match_info[1]}", kv[kv_key])
-                job_array[index] = line
-                kv_find = true
-                break
-            elsif line.match(/#{section}/)
-                section_find = index
-            end
-        end
-
-        if !kv_find    #append at the end or after the section
-            append_info = "#{kv_key}: #{kv[kv_key]}"
-            if section_find == -1
-                job_array << append_info
-            else
-                job_array.insert(section_find + 1, append_info)
-            end
-        end
-
-        File.open(file_path, "w") do |f|
-            job_array.each do |line|
-                f.puts(line)
-            end
-        end
-
-    end
-
     def self.prepare_dir(file_path : String)
         file_path_dir = File.dirname(file_path)
         if !File.exists?(file_path_dir)
@@ -188,11 +152,5 @@ module Jobfile::Operate
             return "#{target_path}/lkp/lkp/src"
         end
         return ENV["LKP_SRC"]
-    end
-
-    def self.load_yaml(file_path : String)
-        File.open(file_path) do |file|
-            YAML.parse(file)
-        end
     end
 end
