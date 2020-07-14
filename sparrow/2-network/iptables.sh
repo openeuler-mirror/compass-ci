f49362cbdb488f17dc05e857edd176277b149bec
Liu Yinsi <liuyinsi@huawei.com>
2020-07-14 19:11:32 +0800

    fix iptables file

--- sparrow/2-network/iptables.sh
+++ sparrow/2-network/iptables.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -e
-if [ ! -f "/etc/firewalld/zones/public.xml" ]
-then
-	exit
-else
-	sed -i '/<zone>/a\<rule family="ipv4">\n  <source address="172.17.0.1/16" />\n  <accept />\n</rule>' /etc/firewalld/zones/public.xml
-	systemctl restart firewalld
-fi
+#!/bin/bash
 
+PUB_IFACE=`ip route get 1.2.3.4 | awk '{print $5; exit}'`
+INT_IFACE=br0
+
+BR0_SUBNET=192.168.177.0/24
+
+#iptables -t nat -F
+iptables -t nat -A POSTROUTING -o $PUB_IFACE -s $BR0_SUBNET -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $INT_IFACE -d $BR0_SUBNET -j MASQUERADE
