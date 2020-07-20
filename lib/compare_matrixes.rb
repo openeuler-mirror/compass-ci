3d15df332b62d8184336152d162d713b9ce348dd
Zhang Yuhang <zhangyuhang25@huawei.com>
2020-07-20 16:09:15 +0800

    Add format output functions
    Signed-off-by: Zhang Yuhang <zhangyuhang25@huawei.com>

--- lib/compare_matrixes.rb
+++ lib/compare_matrixes.rb
@@ -130,3 +130,160 @@ def compare_matrixes(matrixes_list)
   print_result(matrixes_value, matrixes_list.length, false)
   print_result(matrixes_value, matrixes_list.length, true)
 end
+
+# Format Output
+def format_field(field)
+  INTERVAL_BLANK + format("%#{COLUMN_WIDTH}s", field)
+end
+
+def format_runs_fails(runs, fails)
+  runs_width = (SUB_LONG_COLUMN_WIDTH * 3 / 4).to_i
+  fails_width = SUB_LONG_COLUMN_WIDTH - runs_width - 3
+  runs_str = get_suitable_number_str(runs, runs_width, "%#{runs_width}d")
+  fails_str = get_suitable_number_str(fails, fails_width, "%-#{fails_width}d")
+  runs_str + ' : ' + fails_str
+end
+
+def format_reproduction(reproduction)
+  reproduction_str = get_suitable_number_str(reproduction, SUB_SHORT_COLUMN_WIDTH, '%+.1f%%')
+  format("%-#{SUB_SHORT_COLUMN_WIDTH}s", reproduction_str)
+end
+
+def format_change(change)
+  return format("%-#{SUB_SHORT_COLUMN_WIDTH}d", 0) unless change
+
+  change_str = get_suitable_number_str(change, SUB_SHORT_COLUMN_WIDTH - 2, '%+.1f%%')
+  format("%-#{SUB_SHORT_COLUMN_WIDTH}s", change_str)
+end
+
+def get_index(matrixes_number)
+  print_buf = "\n\n\n" + format("%#{SUB_LONG_COLUMN_WIDTH + INTERVAL_WIDTH}d", 0)
+  (1...matrixes_number).each do |index|
+    print_buf += format("%#{COLUMN_WIDTH + INTERVAL_WIDTH}d", index)
+  end
+  print_buf + format("%#{COLUMN_WIDTH + INTERVAL_WIDTH}s\n", FIELD_STR)
+end
+
+def get_liner(matrixes_number)
+  liner_buf = INTERVAL_BLANK + '-' * SUB_LONG_COLUMN_WIDTH
+  (1...matrixes_number).each do |_index|
+    liner_buf += INTERVAL_BLANK + '-' * COLUMN_WIDTH
+  end
+  liner_buf + INTERVAL_BLANK + '-' * COLUMN_WIDTH
+end
+
+def get_title_bar(common_title, compare_title, matrixes_number)
+  print_buf = get_title(common_title, compare_title, matrixes_number)
+  print_buf + get_title_symbol(common_title, compare_title, matrixes_number)
+end
+
+def get_title(common_title, compare_title, matrixes_number)
+  print_column = INTERVAL_BLANK + compare_title
+  print_column += ' ' * (COLUMN_WIDTH - common_title.length - compare_title.length)
+  print_column += common_title
+  format("%#{SUB_LONG_COLUMN_WIDTH + INTERVAL_WIDTH}s", common_title) + print_column \
+    * (matrixes_number - 1) + "\n"
+end
+
+def get_matrixes_header(matrixes_number, success)
+  print_buf = get_index(matrixes_number) + get_liner(matrixes_number) + "\n"
+  if success
+    print_buf + get_title_bar(STDDEV_STR, CHANGE_STR, matrixes_number)
+  else
+    print_buf + get_title_bar(RUNS_FAILS_STR, REPRODUCTION_STR, matrixes_number)
+  end
+end
+
+def get_base_matrix_title_symbol(common_title)
+  print_buf = ' ' * (INTERVAL_WIDTH + SUB_LONG_COLUMN_WIDTH)
+  print_buf[INTERVAL_WIDTH + SUB_LONG_COLUMN_WIDTH - common_title.length / 2] = '\\'
+  print_buf
+end
+
+def get_other_matrixes_title_symbol(common_title, compare_title, matrixes_number)
+  print_buf = ' ' * ((INTERVAL_WIDTH + COLUMN_WIDTH) * (matrixes_number - 1))
+  start_point = 0
+  (1...matrixes_number).each do |_i|
+    start_point += INTERVAL_WIDTH
+    print_buf[start_point + compare_title.length / 2] = '|'
+    start_point += COLUMN_WIDTH
+    print_buf[start_point - common_title.length / 2] = '\\'
+  end
+  print_buf
+end
+
+def get_title_symbol(common_title, compare_title, matrixes_number)
+  get_base_matrix_title_symbol(common_title) + get_other_matrixes_title_symbol(
+    common_title, compare_title, matrixes_number
+  ) + "\n"
+end
+
+def get_suitable_number_str(number, length, format_pattern)
+  # if number string length can't < target length, transform number string to scientific notation string
+  #
+  number_length = length - 5
+  number_length -= 1 if number.negative?
+  return format(format_pattern, number) if Math.log(number.abs, 10) < length || number_length.negative?
+
+  format("%.#{number_length}e", number)
+end
+
+def format_stddev_percent(stddev_percent, average_width)
+  if stddev_percent
+    if stddev_percent.abs != 0
+      percent_width = SUB_LONG_COLUMN_WIDTH - average_width - 4
+      percent_str = get_suitable_number_str(stddev_percent, percent_width, "%#{percent_width}d")
+      # that symbol print width is 2
+      return " ±#{percent_str}%"
+    end
+  end
+  ''
+end
+
+def format_stddev(average, stddev_percent)
+  average_width = (SUB_LONG_COLUMN_WIDTH * STDDEV_AVERAGE_PROPORTION).to_i
+  average_str = get_suitable_number_str(average.round(2), average_width, "%#{average_width}.2f")
+  percent_str = format_stddev_percent(stddev_percent, average_width)
+  format("%-#{SUB_LONG_COLUMN_WIDTH}s", average_str + percent_str)
+end
+
+def get_header(matrixes_number, success)
+  print_buf = get_index(matrixes_number) + get_liner(matrixes_number) + "\n"
+  if success
+    print_buf + get_title_bar(STDDEV_STR, CHANGE_STR, matrixes_number)
+  else
+    print_buf + get_title_bar(RUNS_FAILS_STR, REPRODUCTION_STR, matrixes_number)
+  end
+end
+
+def print_success_values(values, index)
+  print format_change(values['change']) unless index.zero?
+  print format_stddev(values['average'], values['stddev_percent'])
+end
+
+def print_failure_values(values, index)
+  print format_reproduction(values['reproduction']) unless index.zero?
+  print format_runs_fails(values['runs'], values['fails'])
+end
+
+def print_values(matrixes, success)
+  matrixes.each do |index, values|
+    print INTERVAL_BLANK
+    if success
+      print_success_values(values, index)
+    else
+      print_failure_values(values, index)
+    end
+  end
+end
+
+def print_result(matrixes_values, matrixes_number, success)
+  return if matrixes_values[success].empty?
+
+  print get_header(matrixes_number, success)
+  matrixes_values[success].each do |field, matrixes|
+    print_values(matrixes, success)
+    print format_field(field)
+    print "\n"
+  end
+end
