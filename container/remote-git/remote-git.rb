2ee0c09e4507fc2cf91fbe9c825145df4b484b35
Ren Zhiwen <renzhiwen1@huawei.com>
2020-07-22 10:36:49 +0800

    container/remote-git: change request and response params
    - The project parameter and develop_repo parameter are merged into
    git_repo.
    - status change to errcode.
    
    Signed-off-by: Ren Zhiwen <zhiwen.ren1@huawei.com>

--- container/remote-git/remote-git.rb
+++ container/remote-git/remote-git.rb
@@ -16,7 +16,7 @@ post '/git_command' do
   begin
     data = JSON.parse request.body.read
   rescue JSON::ParserError
-    return JSON.dump({ 'status': 100, 'errmsg': 'parse json error' })
+    return JSON.dump({ 'errcode' => '100', 'errmsg' => 'parse json error' })
   end
 
   begin
@@ -27,8 +27,8 @@ post '/git_command' do
     # check if git_command contains illegal characters
     check_illegal_char(data['git_command'])
     # check if git repository exists
-    repo_path = File.join(GIT, data['project'], data['developer_repo'])
-    raise JSON.dump({ 'status': 200, 'errmsg': 'repository not exists' }) unless File.exist?(repo_path)
+    repo_path = File.join(GIT, data['git_repo'])
+    raise JSON.dump({ 'errcode' => '200', 'errmsg' => 'repository not exists' }) unless File.exist?(repo_path)
   rescue StandardError => e
     return e.message
   end
@@ -40,28 +40,27 @@ post '/git_command' do
   err = stderr.read
   exit_code = wait_thr.value.to_i
 
-  { 'status': 0, 'stdout': out, 'stderr': err, 'exit_code': exit_code }.to_json
+  { 'errcode' => '0', 'stdout' => out, 'stderr' => err, 'exit_code' => exit_code }.to_json
 end
 
 def check_git_params(git_command)
-  raise JSON.dump({ 'status': 104, 'errmsg': 'git_command params type error' }) if git_command.class != Array
-  raise JSON.dump({ 'status': 105, 'errmsg': 'git_command length error' }) if git_command.length < 2
-  raise JSON.dump({ 'status': 107, 'errmsg': 'not git-* command' }) unless git_command[0].start_with? 'git-'
+  raise JSON.dump({ 'errcode' => '104', 'errmsg' => 'git_command params type error' }) if git_command.class != Array
+  raise JSON.dump({ 'errcode' => '105', 'errmsg' => 'git_command length error' }) if git_command.length < 2
+  raise JSON.dump({ 'errcode' => '107', 'errmsg' => 'not git-* command' }) unless git_command[0].start_with? 'git-'
 
   git_command[0] = "/usr/lib/git-core/#{git_command[0]}"
   return nil
 end
 
 def check_params_complete(params)
-  raise JSON.dump({ 'status': 101, 'errmsg': 'no project params' }) unless params.key?('project')
-  raise JSON.dump({ 'status': 102, 'errmsg': 'no developer_repo params' }) unless params.key?('developer_repo')
-  raise JSON.dump({ 'status': 103, 'errmsg': 'no git_command params' }) unless params.key?('git_command')
+  raise JSON.dump({ 'errcode' => '101', 'errmsg' => 'no git_repo params' }) unless params.key?('git_repo')
+  raise JSON.dump({ 'errcode' => '102', 'errmsg' => 'no git_command params' }) unless params.key?('git_command')
 end
 
 def check_illegal_char(git_command)
   detected_string = git_command.join
   ILLEGAL_SHELL_CHAR.each do |char|
-    raise JSON.dump({ 'status': 108, 'errmsg': 'git_command params illegal' }) if detected_string.include?(char)
+    raise JSON.dump({ 'errcode' => '108', 'errmsg' => 'git_command params illegal' }) if detected_string.include?(char)
   end
   nil
 end
