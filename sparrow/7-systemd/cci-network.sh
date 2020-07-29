c020d5d68033f73498451ac6b0ecc3b5d7611c73
Shi Zhichao <shizhichao2@huawei.com>
2020-07-29 16:32:59 +0800

    fix: sparrow resume cci after system rebooted
    docker run with option --restart=always already
    while not all containers auto restarted.
    
    Signed-off-by: Shi Zhichao <shizhichao2@huawei.com>

--- sparrow/7-systemd/cci-network.sh
+++ sparrow/7-systemd/cci-network.sh
@@ -7,3 +7,7 @@
 $CCI_SRC/sparrow/2-network/br0.sh
 $CCI_SRC/sparrow/2-network/iptables
 $CCI_SRC/sparrow/2-network/nfs.sh
+
+# --restart=always option is not absolutely reliable
+containers=($(docker ps -a |grep -v NAMES |awk '{print $NF}'))
+[ -n "$containers" ] && docker start ${containers} >/dev/null 2>&1
