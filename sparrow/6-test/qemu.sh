df878bf26f869c29c8b7b10db19f9c8d7521878c
Hu Xuejiao <huxuejiao1@huawei.com>
2020-08-06 18:35:23 +0800

    sparrow/6-test: refactor qemu-kvm test action
    Signed-off-by: Hu Xuejiao <huxuejiao1@huawei.com>

--- sparrow/6-test/qemu.sh
+++ sparrow/6-test/qemu.sh
@@ -2,13 +2,14 @@
 
 run_job()
 {
-	export hostname=$2
-	./0_addjob.sh ../jobs/$1
-	./2_runqemu.sh
+	export tbox_group=$2
+	tj ../jobs/iperf-sparrow.yaml
+	cd /c/crystal-ci/providers
+	./my-qemu.sh
 }
 
 cd /c/cci/user-client/helper
 
 dmidecode -s system-product-name | grep -iq "virtual" && exit
-run_job iperf-pxe.yaml	vm-pxe-hi1620-2p8g-1
-run_job iperf-vm.yaml	vm-hi1620-2p8g-1
+run_job vm-hi1620-2p8g
+run_job vm-pxe-hi1620-2p8g
