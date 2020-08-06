79b6817c3afd7430fed58a098b55d5fc8429c3ee
Cao Xueliang <caoxueliang2@huawei.com>
2020-08-06 19:24:36 +0800

    git-bisect: loop for consume bisect queue
    Cycle every 600s, start 10 threads at every cycle.
    
    Signed-off-by: Cao Xueliang <caoxueliang2@huawei.com>

---
+++ src/git_bisect.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'threadpool'
+require_relative './git-bisect/bisect_worker'
+
+pool = ThreadPool.new(10)
+loop do
+  10.times do
+    pool.process do
+      begin
+        puts 'start consume'
+        bisect_worker = BisectWorker.new
+        bisect_worker.consume_bisect
+      rescue StandardError => e
+        puts e.message
+        puts e.backtrace.inspect
+      end
+    end
+  end
+  sleep(600)
+end
