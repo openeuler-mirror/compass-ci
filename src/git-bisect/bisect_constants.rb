f5998d8b779fde264f5997bde9bc89d790dbb6c5
Cao Xueliang <caoxueliang2@huawei.com>
2020-08-06 19:24:39 +0800

    git-bisect: a constants file for git bisect
    Define some constants for git bisect.
    
    Signed-off-by: Cao Xueliang <caoxueliang2@huawei.com>

---
+++ src/git-bisect/bisect_constants.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+TMEP_GIT_BASE = '/c/public_git'
+BISECT_TASK_QUEUE = 'bisect'
+BISECT_RUN_SCRIPT = "#{ENV['CCI_SRC']}/src/git-bisect/bisect_run_script.rb"
+BISECT_TBOX_GROUP = 'vm-git-bisect'
