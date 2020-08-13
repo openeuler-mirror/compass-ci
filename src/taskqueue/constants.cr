733b06cbcfd3b50057cb87556a2edc5381d37843
Tong Qunfeng <tongqunfeng@huawei.com>
2020-08-13 20:11:50 +0800

    fix: taskqueue: add redis PoolConnect num
    before:
     yesterday:
     - scheduler use 1 redis connect. and when use muti-qemu
     - it raise "Exception: cast from String to Int64 failed"
     - TaskQueue fix this problem by use redis with 16 PoolConect
     so change scheduler use 25 PoolConnect
    
     today:
     - "docker logs -f fluentd | grep 3000" shows 0 scheduler Exception
     - "docker logs -f taskqueue-3060" shows 1 taskqueue Exception
       all request number is 44841
       1 Exception for: "No free connection (used 16 of 16)"
    
    after:
      increase TaskQueue PoolConnect for 16 to 32 (big than scheduler
      PoolConnect num 25)
    
    Signed-off-by: Tong Qunfeng <tongqunfeng@huawei.com>

--- src/taskqueue/constants.cr
+++ src/taskqueue/constants.cr
@@ -6,7 +6,8 @@ QUEUE_NAME_BASE = "queues"
 
 REDIS_HOST = "172.17.0.1"
 REDIS_PORT = 6379
-REDIS_POOL_NUM = 16
+REDIS_POOL_NUM = 32 # when use 16 (scheduler use 25), meet Exception:
+                    #   Exception: No free connection (used 16 of 16)
 
 HTTP_MAX_TIMEOUT = 57000     # less to 1 minute (most http longest timeout)
 HTTP_DEFAULT_TIMEOUT = 300   # 300ms (try every 5ms, that's 60 times of try)
