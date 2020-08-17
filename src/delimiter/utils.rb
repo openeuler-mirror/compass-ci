288e1ce703f39a0e68d9b3e3a85386d08594000b
Cao Xueliang <caoxueliang2@huawei.com>
2020-08-17 10:48:51 +0800

    feat: delimiter: specified date => before date
    Change the specified date commit to the before date commit.
    
    Signed-off-by: Cao Xueliang <caoxueliang2@huawei.com>

--- src/delimiter/utils.rb
+++ src/delimiter/utils.rb
@@ -28,9 +28,8 @@ module Utils
     def get_day_ago_commit(work_dir, commit, day_ago)
       date = `git -C #{work_dir} rev-list --first-parent --pretty=format:%cd \
       --date=short #{commit} -1 | sed -n 2p`.chomp!
-      since = `date -d '-#{day_ago.to_i + 1} day #{date}' +%Y-%m-%d`.chomp!
-      before = `date -d '1 day #{since}' +%Y-%m-%d`.chomp!
-      day_ago_commit = `git -C #{work_dir} rev-list --since=#{since} --before=#{before} \
+      before = `date -d '-#{day_ago} day #{date}' +%Y-%m-%d`.chomp!
+      day_ago_commit = `git -C #{work_dir} rev-list --before=#{before} \
       --pretty=format:%H --first-parent #{commit} -1 | sed -n 2p`.chomp!
       return day_ago_commit
     end
