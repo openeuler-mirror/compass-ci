85909aa6873dfb1d1241e8635b177f62cdaaefa1
Tong Qunfeng <tongqunfeng@huawei.com>
2020-08-18 18:30:38 +0800

    feat: helper: redis GC for manul delete job
    when find 1 job is manual deleted, we can use this script to
      delete associate redis key field
      - scheduler store associate data at sched/id2job
      - taskqueue store associate data at queues/id2content
    
    script main step like this:
    gc = GarbageCollection.new($job_id)
    puts gc.run
    
    may get result like:
     - Not define process now (to alive more than 3 days job)
     - Garbage collection for id=28203 ( to manual deleted job)
     - Doing nothing for id=42000
    
    Signed-off-by: Tong Qunfeng <tongqunfeng@huawei.com>

---
+++ user-client/helper/redis_op_gc/garbage_collection.rb
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: MulanPSL-2.0+
+# frozen_string_literal: true
+
+require_relative './abnormal_id_detect'
+
+# garbage collection for redis key (not exists field)
+# - taskqueue hash key: "queues/id2content"
+# - scheduler hash key: "sched/id2job"
+class GarbageCollection
+  def initialize(id, days = 3, content = nil)
+    @task_id = id
+    @day_number = days
+
+    @content = get_taskqueue_content4id(content, id)
+  end
+
+  def run
+    abn = AbnormalIdDetect.new(@task_id, @day_number, @content.to_json)
+    result = abn.check
+    case result.gsub!(/=.*/, '')
+    when MANUAL_DELETED
+      gc4taskqueue
+      gc4scheduler
+      GC4ID + "=#{@task_id}"
+    when ALIVE_TOO_LONG
+      'No define process now'
+    else
+      GCN4ID + "=#{@task_id}"
+    end
+  end
+
+  def gc4taskqueue
+    cmd = "#{CMD_BASE} queues/id2content , hdel #{@task_id}"
+    `#{cmd}`.chomp
+  end
+
+  def gc4scheduler
+    cmd = "#{CMD_BASE} sched/id2job , hdel #{@task_id}"
+    `#{cmd}`.chomp
+  end
+end
