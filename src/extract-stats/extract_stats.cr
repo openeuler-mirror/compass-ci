d5055f47c791d651ae9be86eeb5885d4d0d80956
Lu Weitao <luweitao2@huawei.com>
2020-08-25 21:10:55 +0800

    fork multiple stats-workers
    fork multiple stats-workers for extract stats flow
    
    Signed-off-by: Lu Weitao <luweitao2@huawei.com>

--- src/extract-stats/extract_stats.cr
+++ src/extract-stats/extract_stats.cr
@@ -7,6 +7,14 @@ module ExtractStats
 
   # Consume scheduler queue
   def self.in_extract_stats()
+    STATS_WORKER_COUNT.times do
+      Process.fork{
+        self.consume_task
+      }
+    end
+  end
+
+  def self.consume_task()
     worker = StatsWorker.new
     worker.consume_sched_queue(EXTRACT_STATS_QUEUE_PATH)
   end
