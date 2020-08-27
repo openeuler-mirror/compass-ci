63dd97b61bd088a546f9cab23b8457e59f1020fa
Bai Jing <baijing14@huawei.com>
2020-08-27 14:58:15 +0800

    overlay-lkp.sh: fix shellcheck warning
    Signed-off-by: Bai Jing <baijing14@huawei.com>

--- container/dracut-initrd/bin/overlay-lkp.sh
+++ container/dracut-initrd/bin/overlay-lkp.sh
@@ -1,45 +1,45 @@
-
+#!/bin/bash
 # transfer LKP dirs
 [  -d /lkp ] || return 0
 
-cp -a /lkp 				$NEWROOT/
+cp -a /lkp 				"$NEWROOT"/
 
 for i in /lkp/lkp/src/rootfs/addon/* /usr/local/* /opt/*
 do
-	dir=$(basename $i)
+	dir=$(basename "$i")
 
-	[  -d $NEWROOT/$dir ] ||
-	mkdir $NEWROOT/$dir
+	[  -d "$NEWROOT/$dir" ] ||
+		mkdir -p "$NEWROOT/$dir"
 	[ "$i" != "${i%/\*}" ] && continue	# skip empty dir
 
-	for j in $i/* $i/.??*
+	for j in "$i"/* "$i"/.??*
 	do
 		[ "$j" != "${j%/\*}" ] && continue	# skip empty dir
 
-		[ -f $j ] && {
-			cp -a $j $NEWROOT/$dir/
+		[ -f "$j" ] && {
+			cp -a "$j" "$NEWROOT/$dir"/
 			continue
 		}
 
-		subdir=$(basename $j)
+		subdir=$(basename "$j")
 
-		[  -d $NEWROOT/$dir/$subdir ] ||
-		mkdir $NEWROOT/$dir/$subdir
+		[  -d "$NEWROOT/$dir/$subdir" ] ||
+			mkdir -p "$NEWROOT/$dir/$subdir"
 
-		for k in $j/*
+		for k in "$j"/*
 		do
 			[ "$k" != "${k%/\*}" ] && continue	# skip empty dir
 
-			cp -a $j/* 	$NEWROOT/$dir/$subdir/
+			cp -a "$j"/* 	"$NEWROOT/$dir/$subdir"/
 		done
 	done
 done
 
 [  -d /usr/src ] &&
-cp -a /usr/src			$NEWROOT/usr/
+	cp -a /usr/src			"$NEWROOT"/usr/
 
 kmdir=/lib/modules/$(uname -r)
-if test -d $kmdir &&  ! test -d $NEWROOT/$kmdir; then
-    cp -an $kmdir		$NEWROOT/lib/modules/
-    cp -an /lib/firmware	$NEWROOT/lib/
+if test -d "$kmdir" &&  ! test -d "$NEWROOT/$kmdir"; then
+    cp -an "$kmdir"		"$NEWROOT"/lib/modules/
+    cp -an /lib/firmware	"$NEWROOT"/lib/
 fi
