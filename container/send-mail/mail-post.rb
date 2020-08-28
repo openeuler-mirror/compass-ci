4c94468d652dabf80c1356311c93008942b3ccaf
Luan Shengde <luanshengde2@huawei.com>
2020-08-28 10:59:20 +0800

    send-mail: add supportted data types
    enable data types: yaml, json, email file
    
    Usage:
    json/yaml content type:
    
            data='{
            "subject": "mail subject",
            "to": "email_to_addr",
            "body": "email message"
            }'
    or
            data="
            subject: mail subject
            to: email_to_addr
            body: email message"
    
            curl -XPOST 'localhost:8101/send_mail_yaml' -d "$data"
    
    Mail content type:
    
            data="
            To: mail_address
            Subject: mail_subject
    
            mail_msg_line1
            mail_msg_line2
            ...
            "
    
            curl -XPOST 'localhost:8101/send_mail_text' -d "$data"
    
    Signed-off-by: Luan Shengde <luanshengde2@huawei.com>

--- container/send-mail/mail-post.rb
+++ container/send-mail/mail-post.rb
@@ -4,31 +4,35 @@
 
 require 'sinatra'
 require 'json'
+require 'yaml'
 require 'open3'
 require_relative 'send-mail.rb'
 
 set :bind, '0.0.0.0'
 set :port, 8101
 
-post '/send_mail' do
-  request.body.rewind
-  begin
-    puts 'hh'
-    data = JSON.parse request.body.read
-  rescue JSON::ParserError
-    return [400, headers.update({ 'errcode' => '100', 'errmsg' => 'parse json error' }), '']
-  end
-  puts '-' * 50
-  puts 'post body:', data
+post '/send_mail_yaml' do
+  data = YAML.safe_load request.body.read
+  raise TypeError, data, 'request data type error' unless data.class.eql? Hash
 
+  subject = data['subject']
   to = data['to']
   body = data['body']
-  subject = data['subject']
-  begin
-    send_mail(subject, to, body)
-  rescue StandardError => e
-    puts 'error message: ', e.message
-    return [400, headers.update(JSON.parse(e.message)), '']
-  end
-  [200, headers.update({ 'errcode' => '0' })]
+  to_send_mail(subject, to, body)
+end
+
+post '/send_mail_text' do
+  data = Mail.read_from_string(request.body.read)
+  subject = data.subject
+  to = data.to
+  body = data.body.decoded
+  to_send_mail(subject, to, body)
+end
+
+def to_send_mail(subject, to, body)
+  raise TypeError, data, 'empty subject.' if subject.empty?
+  raise TypeError, data, 'empty email address.' if to.empty?
+  raise TypeError, data, 'empty email content.' if body.empty?
+
+  send_mail(subject, to, body)
 end
