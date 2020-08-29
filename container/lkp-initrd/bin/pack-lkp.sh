d1b9dbe03b0cf5008cc3b3fab37a3135a1edbb68
Wei Jihui <weijihui2@huawei.com>
2020-08-29 11:00:07 +0800

    container/lkp-initrd: check ARCH and LKP_SRC in advance
    ensure it can create aarch64 or x86_64 package by default config,
    so it need to know ARCH in advance.
    
    Signed-off-by: Wei Jihui <weijihui2@huawei.com>

--- container/lkp-initrd/bin/pack-lkp.sh
+++ container/lkp-initrd/bin/pack-lkp.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -e
 # SPDX-License-Identifier: MulanPSL-2.0+
 
-[[ $ARCH 	]] || ARCH=$(uname -m)
-[[ $LKP_SRC	]] || LKP_SRC=/c/lkp-tests
-
 export OWNER=root.root
 export LKP_USER=lkp
 export USER=lkp
