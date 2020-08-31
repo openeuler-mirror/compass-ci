4d931d23c136f9de859cf4e6f81d511d323bb7f3
Li Ping <liping136@huawei.com>
2020-08-31 20:20:23 +0800

    write a script to wrap some common git command
    Signed-off-by: Li Ping <liping136@huawei.com>

---
+++ lib/git.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# wrap common git commands
+class GitCommit
+  def initialize(repo, commit)
+    @git_prefix = "git -C /srv/git/#{repo}.git"
+    @commit = commit
+  end
+
+  def author_name
+    `#{@git_prefix} log -n1 --pretty=format:'%an' #{@commit}`.chomp
+  end
+
+  def author_email
+    `#{@git_prefix} log -n1 --pretty=format:'%ae' #{@commit}`.chomp
+  end
+
+  def url
+    `#{@git_prefix} remote -v`.split[1]
+  end
+
+  def diff
+    `#{@git_prefix} diff -1 #{@commit}~..#{@commit}`.chomp
+  end
+end
