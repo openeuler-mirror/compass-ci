18939c0a1bedaf827bcaa47ccb70e15a3b23b7a4
Zhang Yu <2134782174@qq.com>
2020-09-01 16:56:02 +0800

    container/dracut-initrd: install kmod in setup-dracut.sh
    When ./build find warning: "/tmp/console-setup.conf.9LaPYs: lsmod not  found"
    
    Signed-off-by: Zhang Yu <zhangyu424@huawei.com>
    Signed-off-by: Zhang Yu <2134782174@qq.com>

--- container/dracut-initrd/bin/setup-dracut.sh
+++ container/dracut-initrd/bin/setup-dracut.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y --no-install-recommends apt-utils >/dev/null 2>&1
 apt-get install -y ca-certificates
-apt-get install -y nfs-common netbase cifs-utils
+apt-get install -y nfs-common netbase cifs-utils kmod
 apt-get install -y dracut dracut-network dracut-config-generic
 
 apt-get clean
