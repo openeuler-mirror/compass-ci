df9bd81d7ef7469aacf830087aed80a545b3712b
Ren Wen <15991987063@163.com>
2020-09-10 17:15:08 +0800

    fix: scheduler: respon -> response
    checked with vimdiff.
    
    Signed-off-by: Ren Wen <15991987063@163.com>

--- src/lib/sched.cr
+++ src/lib/sched.cr
@@ -306,13 +306,13 @@ class Sched
     end
 
     private def get_boot_container(job : Job)
-        respon = Hash(String, String).new
-        respon["docker_image"] = "#{job.docker_image}"
-        respon["lkp"] = "http://#{INITRD_HTTP_HOST}:#{INITRD_HTTP_PORT}/initrd/lkp/#{job.lkp_initrd_user}/lkp-#{job.arch}.cgz"
-        respon["job"] = "http://#{SCHED_HOST}:#{SCHED_PORT}/job_initrd_tmpfs/#{job.id}/job.cgz"
+        response = Hash(String, String).new
+        response["docker_image"] = "#{job.docker_image}"
+        response["lkp"] = "http://#{INITRD_HTTP_HOST}:#{INITRD_HTTP_PORT}/initrd/lkp/#{job.lkp_initrd_user}/lkp-#{job.arch}.cgz"
+        response["job"] = "http://#{SCHED_HOST}:#{SCHED_PORT}/job_initrd_tmpfs/#{job.id}/job.cgz"
 
         puts %({"job_id": "#{job.id}", "job_state": "boot"})
-        return respon.to_json
+        return response.to_json
     end
 
     private def get_boot_grub(job : Job)
@@ -446,30 +446,30 @@ class Sched
 
         initrd_deps, initrd_pkg = get_pp_initrd(job)
 
-        respon = "#!ipxe\n\n"
-        respon += initrd_deps
-        respon += initrd_pkg
+        response = "#!ipxe\n\n"
+        response += initrd_deps
+        response += initrd_pkg
         if job.os_mount == "initramfs"
-            respon += "initrd http://#{INITRD_HTTP_HOST}:#{INITRD_HTTP_PORT}/initrd/osimage/#{job.os_dir}/current\n"
-            respon += "initrd http://#{INITRD_HTTP_HOST}:#{INITRD_HTTP_PORT}/initrd/osimage/#{job.os_dir}/run-ipconfig.cgz\n"
+            response += "initrd http://#{INITRD_HTTP_HOST}:#{INITRD_HTTP_PORT}/initrd/osimage/#{job.os_dir}/current\n"
+            response += "initrd http://#{INITRD_HTTP_HOST}:#{INITRD_HTTP_PORT}/initrd/osimage/#{job.os_dir}/run-ipconfig.cgz\n"
         else
-            respon += "initrd http://#{OS_HTTP_HOST}:#{OS_HTTP_PORT}/os/#{job.os_dir}/initrd.lkp\n"
+            response += "initrd http://#{OS_HTTP_HOST}:#{OS_HTTP_PORT}/os/#{job.os_dir}/initrd.lkp\n"
         end
-        respon += "initrd http://#{INITRD_HTTP_HOST}:#{INITRD_HTTP_PORT}/initrd/lkp/#{job.lkp_initrd_user}/#{initrd_lkp_cgz}\n"
-        respon += "initrd http://#{SCHED_HOST}:#{SCHED_PORT}/job_initrd_tmpfs/#{job.id}/job.cgz\n"
-        respon += "kernel http://#{OS_HTTP_HOST}:#{OS_HTTP_PORT}/os/#{job.os_dir}/vmlinuz user=lkp"
-        respon += " job=/lkp/scheduled/job.yaml RESULT_ROOT=/result/job rootovl ip=dhcp ro"
-        respon += " #{job.kernel_append_root}"
-        respon += add_kernel_console_param(job.os_arch)
+        response += "initrd http://#{INITRD_HTTP_HOST}:#{INITRD_HTTP_PORT}/initrd/lkp/#{job.lkp_initrd_user}/#{initrd_lkp_cgz}\n"
+        response += "initrd http://#{SCHED_HOST}:#{SCHED_PORT}/job_initrd_tmpfs/#{job.id}/job.cgz\n"
+        response += "kernel http://#{OS_HTTP_HOST}:#{OS_HTTP_PORT}/os/#{job.os_dir}/vmlinuz user=lkp"
+        response += " job=/lkp/scheduled/job.yaml RESULT_ROOT=/result/job rootovl ip=dhcp ro"
+        response += " #{job.kernel_append_root}"
+        response += add_kernel_console_param(job.os_arch)
         if job.os_mount == "initramfs"
-          respon += " initrd=#{initrd_lkp_cgz} initrd=job.cgz initrd=run-ipconfig.cgz\n"
+          response += " initrd=#{initrd_lkp_cgz} initrd=job.cgz initrd=run-ipconfig.cgz\n"
         else
-          respon += " initrd=#{initrd_lkp_cgz} initrd=job.cgz\n"
+          response += " initrd=#{initrd_lkp_cgz} initrd=job.cgz\n"
         end
-        respon += "boot\n"
+        response += "boot\n"
 
         puts %({"job_id": "#{job.id}", "job_state": "boot"})
-        return respon
+        return response
     end
 
     def update_job_parameter(env : HTTP::Server::Context)
@@ -535,17 +535,17 @@ class Sched
     def close_job(job_id : String)
         job = @redis.get_job(job_id)
 
-        respon = @es.set_job_content(job)
-        if respon["_id"] == nil
+        response = @es.set_job_content(job)
+        if response["_id"] == nil
             # es update fail, raise exception
             raise "es set job content fail! "
         end
 
-        respon = @task_queue.hand_over_task(
+        response = @task_queue.hand_over_task(
             "sched/#{job.tbox_group}", "extract_stats", job_id
         )
-        if respon[0] != 201
-            raise "#{respon}"
+        if response[0] != 201
+            raise "#{response}"
         end
 
         @redis.remove_finished_job(job_id)
