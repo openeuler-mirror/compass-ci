a769bcfefdea857b764608808cc3f8b055a683df
Lu Kaiyi <2392863668@qq.com>
2020-09-12 17:23:18 +0800

    lib/commit_status.rb: query latest good commit
    query latest good commit by error_id for git-bisect process
    
    1. query all jobs from ES db with bad_job['suite'] and
       bad_job['upstream_repo'] as condition.
    2. filter jobs_list with commit_date field.
    3. jobs_list are grouped and sorted by upstream_commit.
    4. query latest good commit with error_id limited.
    5. interface function: query_latest_good_commit(job, error_id).
    
    Signed-off-by: Lu Kaiyi <2392863668@qq.com>

--- lib/commit_status.rb
+++ lib/commit_status.rb
@@ -14,12 +14,53 @@ def query_commit_status(job, error_id)
   return commit_status
 end
 
+def query_latest_good_commit(job, error_id)
+  items = {
+    'suite' => job['suite'],
+    'upstream_repo' => job['upstream_repo']
+  }
+  jobs_list = query_jobs_from_es(items)
+  jobs_list = filter_jobs_list(jobs_list, job)
+  latest_good_commit = parse_latest_good_commit(jobs_list, error_id)
+  return latest_good_commit
+end
+
 def query_jobs_from_es(items)
   es = ESQuery.new(ES_HOST, ES_PORT)
   result = es.multi_field_query items
   jobs = result['hits']['hits']
   jobs_list = extract_jobs_list(jobs)
   return jobs_list
+end
+
+def filter_jobs_list(jobs_list, bad_job)
+  jobs_list.delete_if do |item|
+    item['id'] == bad_job['id'] ||
+      (!item.key? 'commit_date') ||
+      item['commit_date'] > bad_job['commit_date']
+  end
+end
+
+def parse_latest_good_commit(jobs_list, error_id)
+  return nil if jobs_list.empty?
+
+  commit_hash = {}
+  jobs_list.each do |job|
+    commit_id = job['upstream_commit']
+    commit_hash[commit_id] = [] unless commit_hash.key? commit_id
+    commit_hash[commit_id] << job
+  end
+
+  commit_list = commit_hash.to_a
+  commit_list.sort_by! { |item| item[1][0]['commit_date'] }.reverse!
+
+  commit_list.each do |item|
+    upstream_commit = item[0]
+    commit_jobs_list = item[1]
+    commit_status = parse_jobs_status(commit_jobs_list, error_id)
+    return upstream_commit if commit_status
+  end
+  return nil
 end
 
 def parse_jobs_status(jobs_list, error_id)
