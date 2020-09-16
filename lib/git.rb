bd99ff8a21211e3dcbe7fcfb302bcf67253fe8e6
Li Ping <15396232681@163.com>
2020-09-16 15:50:14 +0800

    add error_id and commit subject
    Signed-off-by: Li Ping <15396232681@163.com>

--- lib/git.rb
+++ lib/git.rb
@@ -16,6 +16,10 @@ class GitCommit
     `#{@git_prefix} log -n1 --pretty=format:'%ae' #{@commit}`.chomp
   end
 
+  def subject
+    `#{@git_prefix} log -n1 --pretty=format:'%s' #{@commit}`.chomp
+  end
+
   def commit_time
     `#{@git_prefix} log -n1 --pretty=format:'%ci' #{@commit}`.chomp
   end
--- lib/mail_bisect_result.rb
+++ lib/mail_bisect_result.rb
@@ -1,34 +1,44 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative 'mail_client.rb'
+require_relative 'mail_client'
+require_relative 'git'
 require 'json'
 
 # compose and send email for bisect result
 class MailBisectResult
   def initialize(bisect_info)
+    @error_id = bisect_info['error_id']
     @repo = bisect_info['repo']
     @commit_id = bisect_info['commit']
+    @git_commit = GitCommit.new(@repo, @commit_id)
   end
 
   def create_send_email
-    parse_commit_info
     compose_mail
     send_mail
   end
 
-  def parse_commit_info
-    git_prefix = "git -C /srv/git/#{@repo}.git"
-    @author = `#{git_prefix} log -n1 --pretty=format:'%an' #{@commit_id}`
-    @email = `#{git_prefix} log -n1 --pretty=format:'%ae' #{@commit_id}`
-    @git_url = `#{git_prefix} remote -v`.split[1]
-    @git_diff = `#{git_prefix} diff --stat -1 #{@commit_id}~..#{@commit_id}`
-  end
-
   def compose_mail
-    subject = "[Crystal-CI] delimit #{@repo} result for commit #{@commit_id}"
-    signature = "Crystal-CI delimit service\nhttps://gitee.com/openeuler/crystal-ci"
-    body = "Hi #{@author},\n\nDelimit for #{@git_url} succeeded.\n\n#{@git_diff}\n\n#{signature}"
-    @hash = { 'to' => @email, 'body' => body, 'subject' => subject }
+    subject = "[Crystal-CI] #{@repo}.git: bisect result"
+    body = <<~BODY
+    Hi #{@git_commit.author_name},
+
+      Bisect completed for
+
+      url: #{@git_commit.url}
+      
+      This is a bisect email from crystal-ci. We met some problems when test with new commits.
+      Would you help to check what happend?
+      After submitting a job we noticed an error response due to the commit:
+
+      commit: #{@commit_id[0..11]} ("#{@git_commit.subject}")
+      error_id: #{@error_id}
+
+    Crystal-CI bisect service
+    https://gitee.com/openeuler/crystal-ci
+    BODY
+    @hash = { 'to' => @git_commit.author_email, 'body' => body, 'subject' => subject }
   end
 
   def send_mail
