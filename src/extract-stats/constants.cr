90a8d20a8fb8266b3225d56cec8740cbb8ef2f4f
Ren Wen <15991987063@163.com>
2020-09-21 17:01:18 +0800

    fix: format: ./src/extract-stats/constants.cr
    Signed-off-by: Ren Wen <15991987063@163.com>

--- src/extract-stats/constants.cr
+++ src/extract-stats/constants.cr
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 
 EXTRACT_STATS_QUEUE_PATH = "extract_stats"
-STATS_WORKER_COUNT = 10
+STATS_WORKER_COUNT       = 10
 
 DELIMITER_TASK_QUEUE = "delimiter"
-ERROR_ID_FILES = %w(build-pkg.json)
+ERROR_ID_FILES       = %w(build-pkg.json)
