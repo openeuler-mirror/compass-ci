bcee3b279db01399d6f830751beef7abcfb835c1
Ren Wen <15991987063@163.com>
2020-09-21 17:01:20 +0800

    fix: format: ./src/extract-stats/regression_client.cr
    Signed-off-by: Ren Wen <15991987063@163.com>

--- src/extract-stats/regression_client.cr
+++ src/extract-stats/regression_client.cr
@@ -1,9 +1,7 @@
 require "elasticsearch-crystal/elasticsearch/api"
 require "../scheduler/constants.cr"
 
-
 class RegressionClient
-
   HOST = (ENV.has_key?("ES_HOST") ? ENV["ES_HOST"] : JOB_ES_HOST)
   PORT = (ENV.has_key?("ES_PORT") ? ENV["ES_PORT"] : JOB_ES_PORT).to_i32
 
@@ -12,28 +10,28 @@ class RegressionClient
   end
 
   def store_error_info(error_id : String, job_id : String)
-     @client.create({
-       :index => "regression",
-       :type => "_doc",
-       :body => {
-         "error_id" => error_id,
-         "job_id" => job_id
-        }
-     })
+    @client.create({
+      :index => "regression",
+      :type  => "_doc",
+      :body  => {
+        "error_id" => error_id,
+        "job_id"   => job_id,
+      },
+    })
   end
 
   def check_error_id(error_id : String)
     query_body = {
       "query" => {
         "term" => {
-          "error_id" => error_id
-        }
-      }
+          "error_id" => error_id,
+        },
+      },
     }
     result = @client.search({
-        :index => "regression",
-        :type => "_doc",
-        :body => query_body
+      :index => "regression",
+      :type  => "_doc",
+      :body  => query_body,
     })
     raise "query failed." unless result["hits"]? || result["hits"]["total"]?
     total = result["hits"]["total"]
