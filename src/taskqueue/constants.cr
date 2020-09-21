b3b76d6d1c5c2e3d65870f4862dd3680b834b80f
Ren Wen <15991987063@163.com>
2020-09-21 17:01:41 +0800

    fix: format: ./src/taskqueue/constants.cr
    Signed-off-by: Ren Wen <15991987063@163.com>

--- src/taskqueue/constants.cr
+++ src/taskqueue/constants.cr
@@ -5,12 +5,12 @@ TASKQUEUE_PORT = 3060
 
 QUEUE_NAME_BASE = "queues"
 
-REDIS_HOST = "172.17.0.1"
-REDIS_PORT = 6379
-REDIS_POOL_NUM = 32 # when use 16 (scheduler use 25), meet Exception:
-                    #   Exception: No free connection (used 16 of 16)
+REDIS_HOST     = "172.17.0.1"
+REDIS_PORT     = 6379
+REDIS_POOL_NUM =   32 # when use 16 (scheduler use 25), meet Exception:
+#   Exception: No free connection (used 16 of 16)
 
-REDIS_POOL_TIMEOUT = 10  # ms
+REDIS_POOL_TIMEOUT = 10 # ms
 
-HTTP_MAX_TIMEOUT = 57000     # less to 1 minute (most http longest timeout)
-HTTP_DEFAULT_TIMEOUT = 1     # 1ms (less than redis timeout, that's no retry)
+HTTP_MAX_TIMEOUT     = 57000 # less to 1 minute (most http longest timeout)
+HTTP_DEFAULT_TIMEOUT =     1 # 1ms (less than redis timeout, that's no retry)
