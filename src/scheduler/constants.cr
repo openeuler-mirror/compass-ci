a4a63c5c0809d8dd052261a8c3255bc7c5ecef32
Ren Wen <15991987063@163.com>
2020-09-21 17:01:28 +0800

    fix: format: ./src/scheduler/constants.cr
    Signed-off-by: Ren Wen <15991987063@163.com>

--- src/scheduler/constants.cr
+++ src/scheduler/constants.cr
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 
-JOB_REDIS_HOST = "172.17.0.1"
-JOB_REDIS_PORT = 6379
+JOB_REDIS_HOST       = "172.17.0.1"
+JOB_REDIS_PORT       = 6379
 JOB_REDIS_PORT_DEBUG = 6380
 
-JOB_ES_HOST = "172.17.0.1"
-JOB_ES_PORT = 9200
+JOB_ES_HOST       = "172.17.0.1"
+JOB_ES_PORT       = 9200
 JOB_ES_PORT_DEBUG = 9201
-JOB_INDEX_TYPE = "jobs/_doc"
+JOB_INDEX_TYPE    = "jobs/_doc"
 
 LAB = (ENV.has_key?("LAB") ? ENV["LAB"] : "nolab")
 
@@ -20,7 +20,7 @@ INITRD_HTTP_PORT = (ENV.has_key?("INITRD_HTTP_PORT") ? ENV["INITRD_HTTP_PORT"] :
 OS_HTTP_HOST = (ENV.has_key?("OS_HTTP_HOST") ? ENV["OS_HTTP_HOST"] : "172.168.131.113")
 OS_HTTP_PORT = (ENV.has_key?("OS_HTTP_PORT") ? ENV["OS_HTTP_PORT"] : 8000).to_i32
 
-SRV_OS = "/srv/os"
+SRV_OS     = "/srv/os"
 SRV_INITRD = "/srv/initrd"
 
 DEMO_JOB = %({"suite":"pixz","testcase":"pixz","category":"benchmark","nr_threads":1,"pixz":null,"job_origin":"jobs/pixz.yaml","testbox":"wfg-e595","arch":"x86_64","tbox_group":"wfg-e595","id":"100","kmsg":null,"boot-time":null,"uptime":null,"iostat":null,"heartbeat":null,"vmstat":null,"numa-numastat":null,"numa-vmstat":null,"numa-meminfo":null,"proc-vmstat":null,"proc-stat":null,"meminfo":null,"slabinfo":null,"interrupts":null,"kconfig":"x86_64-rhel-7.6","compiler":"gcc-7"})
