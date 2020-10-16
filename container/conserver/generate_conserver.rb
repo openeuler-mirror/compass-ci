46a4d1cc71799215e1ca0586ea333db7448ea70d
Cui Yili <2268260388@qq.com>
2020-10-16 18:59:39 +0800

    generate conserver configuration file
    Signed-off-by: cuiyili <2268260388@qq.com>

---
+++ container/conserver/generate_conserver.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# SPDX-License-Identifier: MulanPSL-2.0+
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'yaml'
+
+lab = ENV['lab']
+system 'git clone file:///$CCI_REPOS/lab-$lab.git'
+
+def generate_conserver(lab)
+  return unless File.exist?('conserver-head.cf')
+
+  FileUtils.cp('conserver-head.cf', 'conserver.cf')
+
+  host_dir = "lab-#{lab}/hosts/"
+  return unless Dir.exist?(host_dir)
+
+  Dir.each_child(host_dir) do |host|
+    ipmi_ip = YAML.load_file("#{host_dir}#{host}")['ipmi_ip']
+    next if ipmi_ip.nil?
+
+    console = <<~HEREDOC
+    console #{host} {
+      exec /usr/local/bin/ipmi.sh #{ipmi_ip};
+    }
+    HEREDOC
+    File.open('conserver.cf', 'a') { |f| f << console }
+  end
+end
+
+generate_conserver(lab)
