2f654e2ea9b529c2c0f135c7150d4e9dca290dfd
Luan Shengde <shdluan@163.com>
2020-10-21 15:20:01 +0800

    container/assign-account: answerback-email.rb
    add parameters for answerback-email
    easier for administrator to manully assign jumper account for user with the parameters
      -e,--email email_address:      add email address
      -s,--ssh-pubkey pub_key_file:  add a ssh login pub_key
      -f,--raw-email email_file:      add email address[, pub_key] via email
      file
    
    Usage:
      ruby answerback-email.rb -e email_address [-s pub_key_file]
      ruby answerback-email.rb -f email_file
    
    Signed-off-by: Luan Shengde <luanshengde2@huawei.com>

--- container/assign-account/answerback-email.rb
+++ container/assign-account/answerback-email.rb
@@ -3,74 +3,89 @@
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
 # frozen_string_literal: true
 
-=begin
-
-repo_list:
-  all repos url list from upstream-repos.git
-
-API:
-call graph:
-read_mail_content
-send_account_request
-  email_addr
-    get email address
-  email_message_id
-    get email message_id
-  check_email_available
-    check email available
-  pub_key_value
-    get pub key
-  account
-    send apply account request and return account info
-  build_message
-    build email message
-  send_mail
-    call send_mail to send mail whit build message
-
-the returned data for account_info like:
-{
-  "account" => "guest",
-  "passwd" => "Use pub_key to login",
-  "jumper_ip" => "10.10.10.10",
-  "jumper_port" => "10000"
-}
-=end
+# xx --email xxx --login --ssh-pubkey xxx --raw-email email-file
+# samba mount
+# ssh logshn (huawei, ) (install pubkey / send password)
 
 require 'json'
 require 'mail'
 require 'set'
-require_relative '../defconfig.rb'
+require 'optparse'
+require_relative '../defconfig'
 
 names = Set.new %w[
-  JUMPER_IP
+  JUMPER_HOST
   JUMPER_PORT
-  CRYSTAL_INTRANET
-  SEND_MAIL_PORT
+  SEND_MAIL_HOST_INTERNET
+  SEND_MAIL_PORT_INTERNET
 ]
 
 defaults = relevant_defaults(names)
 
-JUMPER_IP = defaults['JUMPER_IP']
-JUMPER_PORT = defaults['JUMPER_PORT']
-CRYSTAL_INTRANET = defaults['CRYSTAL_INTRANET']
-SEND_MAIL_PORT = defaults['SEND_MAIL_PORT']
+JUMPER_HOST = defaults['JUMPER_HOST'] || 'api.compass-ci.openeuler.org'
+JUMPER_PORT = defaults['JUMPER_PORT'] || 29999
+SEND_MAIL_HOST = defaults['SEND_MAIL_HOST_INTERNET'] || 'localhost'
+SEND_MAIL_PORT = defaults['SEND_MAIL_PORT_INTERNET'] || 11312
 
-def build_message(email, message_id, infos)
+$apply_info = {
+  'my_email' => nil,
+  'my_ssh_pubkey' => nil
+}
+
+def init_info(email_file)
+  mail_content = Mail.read(email_file)
+
+  $apply_info['my_email'] = mail_content.from[0]
+  $apply_info['my_ssh_pubkey'] = if mail_content.part[1].filename == 'id_rsa.pub'
+                                   mail_content.part[1].body.decoded.gsub(/\r|\n/, '')
+                                 end
+
+  $apply_info
+end
+
+options = OptionParser.new do |opts|
+  opts.banner = "Usage: answerback-mail.rb [--email email] [--ssh-pubkey pub_key_file] [--raw-email email_file]\n"
+  opts.banner += "       -e or -f is required\n"
+  opts.banner += '       -s is optional when use -e'
+
+  opts.separator ''
+  opts.separator 'options:'
+
+  opts.on('-e|--email email_address', 'appoint email address') do |email_address|
+    $apply_info['my_email'] = email_address
+  end
+
+  opts.on('-s|--ssh-pubkey pub_key_file', 'ssh pub_key file, enable password-less login') do |pub_key_file|
+    $apply_info['my_ssh_pubkey'] = File.read(pub_key_file)
+  end
+
+  opts.on('-f|--raw-email email_file', 'email file') do |email_file|
+    init_info(email_file)
+  end
+
+  opts.on_tail('-h|--help', 'show this message') do
+    puts opts
+    exit
+  end
+end
+
+options.parse!(ARGV)
+
+def build_message(email, acct_infos)
   message = <<~EMAIL_MESSAGE
     To: #{email}
-    Message-ID: #{message_id}
     Subject: jumper account is ready
 
-    Dear #{email}
+    Dear user:
 
       Thank you for joining us.
       You can use the following command to login the jumper server:
 
       login command:
-        ssh -p #{infos['jumper_port']} #{infos['account']}@#{infos['jumper_ip']}
+        ssh -p #{acct_infos['jumper_port']} #{acct_infos['account']}@#{acct_infos['jumper_ip']}
 
-      account passwd:
-        account_password: #{infos['passwd']}
+      account password:
+        #{acct_infos['passwd']}
 
     regards
     compass-ci
@@ -79,67 +94,26 @@ def build_message(email, message_id, infos)
   return message
 end
 
-def email_addr(mail_content)
-  msg = 'not an applying account email'
-
-  raise msg unless mail_content.subject =~ /apply ssh account/i
-
-  email = mail_content.from.join(',')
-
-  return email
-end
-
-# def check_email_available(mail_content, email)
-#   oos_list = File.read('/c/upstream-repos/repo_list').split(/\n/)
-#   url = mail_content.body.decoded.split(/\n/).find { |line| line =~ /https?:\/\// }
-#   base_url = url.split('/')[0,5].join('/')
-#   message = 'The url is not in upstream repo_list'
-#
-#   raise message unless oos_list.include? base_url
-#
-#   url_fdback = %x(curl #{url})
-#   email_index = url_fdback.index email
-#
-#   message = 'No commit info found from the url for the email'
-#   raise message unless email_index
-# end
-
-def email_message_id(mail_content)
-  message_id = mail_content.message_id
-  return message_id
-end
-
-def pub_key_value(mail_content)
-  pub_key = mail_content.body.decoded.split(/\n/).find { |line| line =~ /ssh-rsa/ }
-  return pub_key
-end
-
 def account_info(pub_key)
-  account_info_str = %x(curl -XGET '#{JUMPER_IP}:#{JUMPER_PORT}/assign_account' -d "pub_key: #{pub_key}")
-  account_info = JSON.parse account_info_str
-
-  return account_info
+  account_info_str = if pub_key.nil?
+                       %x(curl -XGET '#{JUMPER_HOST}:#{JUMPER_PORT}/assign_account')
+                     else
+                       %x(curl -XGET '#{JUMPER_HOST}:#{JUMPER_PORT}/assign_account' -d "pub_key: #{pub_key}")
+                     end
+  JSON.parse account_info_str
 end
 
-def send_account(mail_content)
-  email = email_addr(mail_content)
-  message_id = email_message_id(mail_content)
-  # check_email_available(mail_content, email)
+def send_account
+  message = "No email address specified\n"
+  message += "use -e email_address add a email address\n"
+  message += 'or use -f to add a email file'
+  raise message if $apply_info['my_email'].nil?
 
-  pub_key = pub_key_value(mail_content)
-  acct_info = account_info(pub_key)
+  acct_info = account_info($apply_info['my_ssh_pubkey'])
 
-  message = build_message(email, message_id, acct_info)
+  message = build_message($apply_info['my_email'], acct_info)
 
-  %x(curl -XPOST '#{CRYSTAL_INTRANET}:#{SEND_MAIL_PORT}/send_mail_text' -d "#{message}")
+  %x(curl -XPOST '#{SEND_MAIL_HOST}:#{SEND_MAIL_PORT}/send_mail_text' -d "#{message}")
 end
 
-def read_mail_content(mail_file)
-  mail_content = Mail.read(mail_file)
-
-  return mail_content
-end
-
-mail_file = ARGV[0]
-mail_content = read_mail_content(mail_file)
-send_account(mail_content)
+send_account
