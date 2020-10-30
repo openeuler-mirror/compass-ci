4557c4d751bd78b9a58dc54eb01050fa595df29e
Wu Zhende <wuzhende666@163.com>
2020-10-30 11:43:27 +0800

    monitoring/amqp.cr: the retry mechanism is added to rabbitmq
    [Why]
      If the rabbitmq service is disconnected,
      monitoring service can't work.
      When the rabbitmq service restored,need to restart monitoring service.
      So need to let monitoring service automatic reconnection
    
    Signed-off-by: Wu Zhende <wuzhende666@163.com>

--- src/monitoring/amqp.cr
+++ src/monitoring/amqp.cr
@@ -16,18 +16,29 @@ class MessageQueueClient
     conn.try &.close
   end
 
-  def monitoring_message_queue(filter : Filter, exchange_name : String, queue_name : String)
-    start do |conn|
-      conn.channel do |channel|
-        queue = channel.queue(queue_name)
-        queue.bind(exchange_name, "")
-        queue.subscribe(tag: queue_name, block: true) do |msg|
-          begin
-            filter.filter_msg(msg.body_io)
-          rescue e
-            puts "filter message error: #{e}"
-          end
+  private def filter_msg(conn, filter, exchange_name, queue_name)
+    conn.channel do |channel|
+      queue = channel.queue(queue_name)
+      queue.bind(exchange_name, "")
+      queue.subscribe(tag: queue_name, block: true) do |msg|
+        begin
+          filter.filter_msg(msg.body_io)
+        rescue e
+          puts "filter message error: #{e}"
         end
+      end
+    end
+  end
+
+  def monitoring_message_queue(filter : Filter, exchange_name : String, queue_name : String)
+    loop do
+      begin
+        start do |conn|
+          filter_msg(conn, filter, exchange_name, queue_name)
+        end
+      rescue e
+        puts "monitoring_message_queue error: #{e}"
+        sleep 5
       end
     end
   end
