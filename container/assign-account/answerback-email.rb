0f9f52e7c0a2163c33b68727d8660d150e3fef6c
Luan Shengde <shdluan@163.com>
2020-11-10 10:36:13 +0800

    assign-account: add my_info for request data
    add my_email, my_name, my_uuid in assign-account request data
    
    [why]
         my_email, my_name, my_uuid is required to config the default yaml
         file
           ~/.config/compass-ci/defaults/account.yaml
    
    [how]
         add my_email, my_name, my_uuid in assign-account request data when
         sending assign-account request.
    
    Signed-off-by: Luan Shengde <shdluan@163.com>

--- container/assign-account/answerback-email.rb
+++ container/assign-account/answerback-email.rb
@@ -12,58 +12,63 @@ require 'mail'
 require 'set'
 require 'optparse'
 require_relative '../defconfig'
+require_relative '../../lib/es_client'
 
 names = Set.new %w[
   JUMPER_HOST
   JUMPER_PORT
-  SEND_MAIL_HOST_INTERNET
-  SEND_MAIL_PORT_INTERNET
+  SEND_MAIL_HOST
+  SEND_MAIL_PORT
 ]
 
 defaults = relevant_defaults(names)
 
 JUMPER_HOST = defaults['JUMPER_HOST'] || 'api.compass-ci.openeuler.org'
 JUMPER_PORT = defaults['JUMPER_PORT'] || 29999
-SEND_MAIL_HOST = defaults['SEND_MAIL_HOST_INTERNET'] || 'localhost'
-SEND_MAIL_PORT = defaults['SEND_MAIL_PORT_INTERNET'] || 11312
+SEND_MAIL_HOST = defaults['SEND_MAIL_HOST'] || 'localhost'
+SEND_MAIL_PORT = defaults['SEND_MAIL_PORT'] || 49000
 
-$apply_info = {
+my_info = {
   'my_email' => nil,
+  'my_name' => nil,
+  'my_uuid' => %x(uuidgen).chomp,
   'my_ssh_pubkey' => nil
 }
 
-def init_info(email_file)
+def init_info(email_file, my_info)
   mail_content = Mail.read(email_file)
-
-  $apply_info['my_email'] = mail_content.from[0]
-  $apply_info['my_ssh_pubkey'] = if mail_content.part[1].filename == 'id_rsa.pub'
-                                   mail_content.part[1].body.decoded.gsub(/\r|\n/, '')
-                                 end
-
-  $apply_info
+  my_info['my_email'] = mail_content.from[0]
+  my_info['my_name'] = mail_content.From.unparsed_value.gsub(/ <[^<>]*>/, '')
+  my_info['my_ssh_pubkey'] = if mail_content.part[1].filename == 'id_rsa.pub'
+                               mail_content.part[1].body.decoded
+                             end
 end
 
 options = OptionParser.new do |opts|
-  opts.banner = "Usage: answerback-mail.rb [--email email] [--ssh-pubkey pub_key_file] [--raw-email email_file]\n"
+  opts.banner = 'Usage: answerback-mail.rb [-e|--email email] '
+  opts.banner += "[-s|--ssh-pubkey pub_key_file] [-f|--raw-email email_file]\n"
   opts.banner += "       -e or -f is required\n"
   opts.banner += '       -s is optional when use -e'
 
   opts.separator ''
   opts.separator 'options:'
 
-  opts.on('-e|--email email_address', 'appoint email address') do |email_address|
-    $apply_info['my_email'] = email_address
+  opts.on('-e email_address', '--email email_address', 'appoint email address') do |email_address|
+    my_info['my_email'] = email_address
+    # when apply account with email address, will get no user name
+    my_info['my_name'] = ''
   end
 
-  opts.on('-s|--ssh-pubkey pub_key_file', 'ssh pub_key file, enable password-less login') do |pub_key_file|
-    $apply_info['my_ssh_pubkey'] = File.read(pub_key_file)
+  opts.on('-s pub_key_file', '--ssh-pubkey pub_key_file', \
+          'ssh pub_key file, enable password-less login') do |pub_key_file|
+    my_info['my_ssh_pubkey'] = File.read(pub_key_file)
   end
 
-  opts.on('-f|--raw-email email_file', 'email file') do |email_file|
-    init_info(email_file)
+  opts.on('-f email_file', '--raw-email email_file', 'email file') do |email_file|
+    init_info(email_file, my_info)
   end
 
-  opts.on_tail('-h|--help', 'show this message') do
+  opts.on_tail('-h', '--help', 'show this message') do
     puts opts
     exit
   end
@@ -71,21 +76,24 @@ end
 
 options.parse!(ARGV)
 
-def build_message(email, acct_infos)
+def build_message(email, account_info)
   message = <<~EMAIL_MESSAGE
     To: #{email}
-    Subject: jumper account is ready
+    Subject: [compass-ci] jumper account is ready
 
     Dear user:
 
       Thank you for joining us.
       You can use the following command to login the jumper server:
 
-      login command:
-        ssh -p #{acct_infos['jumper_port']} #{acct_infos['account']}@#{acct_infos['jumper_ip']}
+      Login command:
+        ssh -p #{account_info['jumper_port']} #{account_info['my_login_name']}@#{account_info['jumper_host']}
 
-      account password:
-        #{acct_infos['passwd']}
+      Account password:
+        #{account_info['my_password']}
+
+      Suggest:
+        If you use the password to login, change it in time.
 
     regards
     compass-ci
@@ -94,26 +102,32 @@ def build_message(email, acct_infos)
   return message
 end
 
-def account_info(pub_key)
-  account_info_str = if pub_key.nil?
-                       %x(curl -XGET '#{JUMPER_HOST}:#{JUMPER_PORT}/assign_account')
-                     else
-                       %x(curl -XGET '#{JUMPER_HOST}:#{JUMPER_PORT}/assign_account' -d "pub_key: #{pub_key}")
-                     end
+def apply_account(my_info)
+  account_info_str = %x(curl -XGET '#{JUMPER_HOST}:#{JUMPER_PORT}/assign_account' -d '#{my_info.to_json}')
   JSON.parse account_info_str
 end
 
-def send_account
+def send_account(my_info)
   message = "No email address specified\n"
-  message += "use -e email_address add a email address\n"
+  message += "use -e to add a email address\n"
   message += 'or use -f to add a email file'
-  raise message if $apply_info['my_email'].nil?
+  raise message if my_info['my_email'].nil?
 
-  acct_info = account_info($apply_info['my_ssh_pubkey'])
-
-  message = build_message($apply_info['my_email'], acct_info)
+  account_info = apply_account(my_info)
+  # for manually assign account, there will be no my_commit_url
+  # but the key my_commit_url is required for es
+  my_info['my_commit_url'] = ''
+  my_info['my_login_name'] = account_info['my_login_name']
+  my_info.delete 'my_ssh_pubkey'
+  store_account_info(my_info)
+  message = build_message(my_info['my_email'], account_info)
 
   %x(curl -XPOST '#{SEND_MAIL_HOST}:#{SEND_MAIL_PORT}/send_mail_text' -d "#{message}")
 end
 
-send_account
+def store_account_info(my_info)
+  es = ESClient.new(index: 'accounts')
+  es.put_source_by_id(my_info['my_email'], my_info)
+end
+
+send_account(my_info)
