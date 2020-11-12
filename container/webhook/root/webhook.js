444ff33be814e8ea84f2c7741a17a9d0095f8e3a
Li Yuanchao <lyc163mail@163.com>
2020-11-12 19:33:56 +0800

    webhook.js: delete url in comment
    Signed-off-by: Li Yuanchao <lyc163mail@163.com>

--- container/webhook/root/webhook.js
+++ container/webhook/root/webhook.js
@@ -1,8 +1,6 @@
 const http = require('http')
 const spawn = require('child_process').spawn
 const createHandler = require('git-webhook-handler')
-// if users want to use webhook, they need to config webhook on their code warehouse('gitee.com' or 'github.com'),
-// and use url: http://183.134.196.212:11301/webhook, secret: webhook@git.
 const handler = createHandler({ path: '/webhook', secret: 'webhook@git' })
 
 handler.on('error', function(err){
