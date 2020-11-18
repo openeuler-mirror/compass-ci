db00a1f6042e2f5e889ded2e1e6695289a68b67b
Luan Shengde <shdluan@163.com>
2020-11-18 17:43:44 +0800

    mail-robot: listen and handle new email
    listen the mailbox for new email
      handle_new_email
        handle the email with defined opetions
        - apply_account
          email subject: apply account
    
    Signed-off-by: Luan Shengde <shdluan@163.com>

---
+++ container/mail-robot/mail-robot.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# SPDX-License-Identifier: MulanPSL-2.0+
+# Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
+# frozen_string_literal: true
+
+require 'json'
+require 'yaml'
+require 'listen'
+require 'mail'
+require 'fileutils'
+require_relative 'lib/apply-account'
+
+MAILDIR = ENV['MAILDIR']
+
+def monitor_new_email(mail_inbox, mail_drafts)
+  listener = Listen.to(mail_inbox) do |_modified, added, _removed|
+    next if added.empty?
+
+    added.each do |mail_file|
+      handle_new_email(mail_file, mail_drafts)
+    end
+  end
+  listener.start
+  sleep
+end
+
+def handle_new_email(mail_file, mail_drafts)
+  mail_content = Mail.read(mail_file)
+  apply_account(mail_content)
+
+  FileUtils.mv(mail_file, mail_drafts)
+end
+
+def apply_account(mail_content)
+  return unless mail_content.subject == 'apply account'
+
+  assign_uuid = ApplyAccount.new(mail_content)
+  assign_uuid.check_to_send_account
+end
+
+monitor_new_email("#{MAILDIR}/new", "#{MAILDIR}/cur")
