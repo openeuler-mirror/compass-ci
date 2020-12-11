805730a3621be5ec0d7c6d1ef858779a749ca00a
Ren Wen <15991987063@163.com>
2020-12-11 10:47:22 +0800

    scheduler: subqueue: change queue to queue/subqueue
    Signed-off-by: Ren Wen <15991987063@163.com>

--- src/scheduler/close_job.cr
+++ src/scheduler/close_job.cr
@@ -14,7 +14,7 @@ class Sched
     end
 
     response = @task_queue.hand_over_task(
-      "sched/#{job.queue}", "extract_stats", job_id
+      "sched/#{job.queue}/#{job.subqueue}", "extract_stats", job_id
     )
     if response[0] != 201
       raise "#{response}"
