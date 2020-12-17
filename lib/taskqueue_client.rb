9b81c6edb3d2a0f3fd6bd9df7abac4513e55408b
Cao Xueliang <caoxl78320@163.com>
2020-12-17 10:43:45 +0800

    lib: add the add_task function to taskqueue client
    1. Modify the default host to 172.17.0.1.
    2. Add the add_task function to taskqueue client.
    
    Signed-off-by: Cao Xueliang <caoxl78320@163.com>

--- lib/taskqueue_client.rb
+++ lib/taskqueue_client.rb
@@ -6,7 +6,7 @@ require 'rest-client'
 
 # taskqueue client
 class TaskQueueClient
-  HOST = (ENV.key?('TASKQUEUE_HOST') ? ENV['TASKQUEUE_HOST'] : '127.0.0.1')
+  HOST = (ENV.key?('TASKQUEUE_HOST') ? ENV['TASKQUEUE_HOST'] : '172.17.0.1')
   PORT = (ENV.key?('TASKQUEUE_PORT') ? ENV['TASKQUEUE_PORT'] : 3060).to_i
   def initialize(host = HOST, port = PORT)
     @host = host
@@ -18,6 +18,16 @@ class TaskQueueClient
     RestClient::Request.execute(
       method: :put,
       url: url
+    )
+  end
+
+  def add_task(queue_path, json_data)
+    url = "http://#{@host}:#{@port}/add?queue=#{queue_path}"
+    RestClient::Request.execute(
+      method: :post,
+      url: url,
+      payload: json_data,
+      headers: { content_type: 'application/json' }
     )
   end
 end
