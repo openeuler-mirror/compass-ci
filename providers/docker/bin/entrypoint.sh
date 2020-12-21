a6fa6976d965139de729cd1abf037a04ba994874
Xiao Shenwei <xiaoshenwei96@163.com>
2020-12-21 10:13:37 +0800

    providers/docker: set the private key of the testbox
    [why]
    login the docker testbox without password
    
    [how]
    Add ssh private key for docker, for automatically establish the ssh
    reverse tunnel between sut and port map container.
    
    Signed-off-by: Xiao Shenwei <xiaoshenwei96@163.com>

--- providers/docker/bin/entrypoint.sh
+++ providers/docker/bin/entrypoint.sh
@@ -2,4 +2,6 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
 
+mkdir -p /root/.ssh
+cp /lkp/lkp/src/rootfs/addon/root/.ssh/id_rsa /root/.ssh/
 sh /lkp/lkp/src/rootfs/addon/etc/init.d/lkp-bootstrap
