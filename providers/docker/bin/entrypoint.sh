4ea8261a409a2895c86dc84094278655527c3b4b
Xiao Shenwei <xiaoshenwei96@163.com>
2021-01-06 18:33:00 +0800

    providers/docker: copy rootfs/addon/root/. to container
    Signed-off-by: Xiao Shenwei <xiaoshenwei96@163.com>

--- providers/docker/bin/entrypoint.sh
+++ providers/docker/bin/entrypoint.sh
@@ -3,6 +3,6 @@
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
 
 mkdir -p /root/.ssh
-cp /lkp/lkp/src/rootfs/addon/root/.ssh/id_rsa /root/.ssh/
+cp -r /lkp/lkp/src/rootfs/addon/root/. /root
 cp -r /lkp/lkp/src/rootfs/addon/usr/* /usr
 sh /lkp/lkp/src/rootfs/addon/etc/init.d/lkp-bootstrap
