c9ee2e77aa21a561fcdff79b3501ef267b988269
Luan Shengde <shdluan@163.com>
2021-02-03 09:14:38 +0800

    sbin/build-my-info: optimize
    optimize options:
      add option -t: generate new my_token
      add option -h: output help message
    
    add search my_info from ES and update my_info before store it.
    
    Signed-off-by: Luan Shengde <shdluan@163.com>

--- sbin/build-my-info.rb
+++ sbin/build-my-info.rb
@@ -8,26 +8,48 @@ require_relative '../lib/build_my_info_client'
 require 'optparse'
 
 option = {
-  my_name: `git config --global user.name`.chomp,
-  my_email: `git config --global user.email`.chomp,
-  lab: `awk '/^lab:\s/ {print $2; exit}' /etc/compass-ci/defaults/*.yaml`.chomp
+  'my_email' => `git config --global user.email`.chomp,
+  'my_name' => `git config --global user.name`.chomp,
+  'lab' => `awk '/^lab:\s/ {print $2; exit}' /etc/compass-ci/defaults/*.yaml`.chomp
 }
 
 options = OptionParser.new do |opts|
+  opts.banner = "Usage: build-my-info [-e email] [-n name] [-l lab] [-t]\n"
+
+  opts.separator ''
+  opts.separator 'options:'
+
   opts.on('-e email', 'my_email') do |email|
-    option[:my_email] = email
+    option['my_email'] = email
   end
 
   opts.on('-n name', 'my_name') do |name|
-    option[:my_name] = name
+    option['my_name'] = name
   end
 
   opts.on('-l lab', 'lab') do |lab|
-    option[:lab] = lab
+    option['lab'] = lab
+  end
+
+  opts.on('-t', 'my_token') do
+    option['my_token'] = %x(uuidgen).chomp
+  end
+
+  opts.on_tail('-h', 'show this message') do
+    puts opts
+    exit
   end
 end
 
 options.parse!
 
-build_my_info = BuildMyInfo.new(option[:my_email], option[:my_name], option[:lab])
-build_my_info.config_my_info
+def build_my_info(option)
+  build_my_info = BuildMyInfo.new(option['my_email'])
+  info_es = build_my_info.search_my_info
+  info_es.update option
+  info_es['my_token'] = %x(uuidgen).chomp if info_es['my_token'].nil?
+
+  build_my_info.config_my_info(info_es)
+end
+
+build_my_info(option)
