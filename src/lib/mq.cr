3a1d7cdd7b464c214d36188f97019c509ec64b0e
Wu Zhende <wuzhende666@163.com>
2021-03-15 14:37:18 +0800

    lib/mq.cr: add new function reconnect
    When the connection to the mq is disconnected,
    an interface needs to be provided to re-initiate the connection.
    
    Signed-off-by: Wu Zhende <wuzhende666@163.com>

--- src/lib/mq.cr
+++ src/lib/mq.cr
@@ -5,15 +5,32 @@ require "singleton"
 require "amqp-client"
 require "amq-protocol"
 
+require "./json_logger"
+
 class MQClient
   getter ch : AMQP::Client::Channel
   MQ_HOST = ENV.has_key?("MQ_HOST") ? ENV["MQ_HOST"] : "172.17.0.1"
   MQ_PORT = (ENV.has_key?("MQ_PORT") ? ENV["MQ_PORT"] : 5672).to_i32
 
   def initialize(host = MQ_HOST, port = MQ_PORT)
+    @log = JSONLogger.new
     @client = AMQP::Client.new("amqp://#{host}:#{port}")
     conn = @client.connect
     @ch = conn.channel.as(AMQP::Client::Channel)
+  end
+
+  def reconnect
+    conn = @client.connect
+    @ch = conn.channel.as(AMQP::Client::Channel)
+    @log.info({
+      "msg" => "rabbitmq reconnected successfully",
+      "source" => "mq_client"
+    }.to_json)
+  rescue e
+    @log.warn({
+      "msg" => e.to_s,
+      "source" => "mq_client"
+    }.to_json)
   end
 
   def self.instance
