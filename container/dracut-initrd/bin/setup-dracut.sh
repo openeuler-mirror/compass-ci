8856c0cba6eb3ffb4dcdd54d6f0213b349c422d1
Xu Xijian <hdxuxijian@163.com>
2021-03-24 10:22:53 +0800

    container/dracut-initrd: add some tools into dracut to enable initrd
    Signed-off-by: Xu Xijian <hdxuxijian@163.com>

--- container/dracut-initrd/bin/setup-dracut.sh
+++ container/dracut-initrd/bin/setup-dracut.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y --fix-missing --no-install-recommends -qq apt-utils \
 nfs-common netbase cifs-utils kmod rsync dracut dracut-network xz-utils \
-dracut-config-generic
+dracut-config-generic lvm2 xz-utils systemd-sysv
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
@@ -17,7 +17,8 @@ cp -a /usr/local/bin/cifs-lib.sh /usr/lib/dracut/modules.d/95cifs/
 
 cat overlay-lkp.sh   >> /usr/lib/dracut/modules.d/90overlay-root/overlay-mount.sh
 sed -i "/install() {/a\    inst /usr/bin/awk" /usr/lib/dracut/modules.d/40network/module-setup.sh
-sed -i "/install() {/a\    inst /sbin/e2fsck /sbin/mke2fs /usr/bin/basename" /usr/lib/dracut/modules.d/00bash/module-setup.sh
+tools_00bash="/sbin/e2fsck /sbin/mke2fs /usr/bin/basename /sbin/lvm /sbin/reboot"
+sed -i "/install() {/a\    inst $tools_00bash" /usr/lib/dracut/modules.d/00bash/module-setup.sh
 
 pre_mount_file="/usr/lib/dracut/modules.d/98dracut-systemd/dracut-pre-mount.sh"
 [ "$(sed -n '$p' $pre_mount_file)"  = "exit 0" ] && sed -i '$d' "$pre_mount_file"
