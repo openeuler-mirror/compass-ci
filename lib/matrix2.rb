5f9b23aebda7e0f67d094660050ccdb02c1d6e0e
Lu Weitao <luweitaobe@163.com>
2021-03-31 15:39:25 +0800

    lib/matrix2.rb: fix NoMethodError
    [Why]
    linjiaxin@crystal ~/crystal.116494% ~/compass-ci/sbin/result2stats ./
    Traceback (most recent call last):
            7: from /home/linjiaxin/compass-ci/sbin/result2stats:38:in `<main>'
            6: from /home/linjiaxin/compass-ci/lib/matrix2.rb:52:in `create_stats'
            5: from /home/linjiaxin/compass-ci/lib/matrix2.rb:52:in `each'
            4: from /home/linjiaxin/compass-ci/lib/matrix2.rb:58:in `block in create_stats'
            3: from /home/linjiaxin/compass-ci/lib/matrix2.rb:30:in `extract_pre_result'
            2: from /home/linjiaxin/compass-ci/lib/matrix2.rb:30:in `each'
            1: from /home/linjiaxin/compass-ci/lib/matrix2.rb:33:in `block in extract_pre_result'
    /home/linjiaxin/compass-ci/lib/matrix2.rb:22:in `set_pre_value': undefined method `/' for #<String:0x0000aaabcdd1a140> (NoMethodError)
    
    Signed-off-by: Lu Weitao <luweitaobe@163.com>

--- lib/matrix2.rb
+++ lib/matrix2.rb
@@ -10,7 +10,7 @@ require "#{LKP_SRC}/lib/matrix"
 require_relative './params_group.rb'
 
 def set_pre_value(item, value, sample_size)
-  return value if value.is_a?(String)
+  return value if item.end_with?('.message', '.log')
 
   if value.size == 1
     value[0]
