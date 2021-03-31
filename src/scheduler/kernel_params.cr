7b18c8b6ee8ef8ad6ac11b358d91d6c5498f2c39
Xu Xijian <hdxuxijian@163.com>
2021-03-31 11:09:56 +0800

    src/scheduler/kernel_params.cr: add param pv_device into dracut initrd for local mount
    Signed-off-by: Xu Xijian <hdxuxijian@163.com>

--- src/scheduler/kernel_params.cr
+++ src/scheduler/kernel_params.cr
@@ -9,7 +9,8 @@ class Job
     os_info = "#{os}_#{os_arch}_#{os_version}"
     use_root_partition = "/dev/mapper/os-#{os_info}_#{src_lv_suffix}" if @hash["src_lv_suffix"]? != nil
     save_root_partition = "/dev/mapper/os-#{os_info}_#{boot_lv_suffix}" if @hash["boot_lv_suffix"]? != nil
-    return "#{common_params} local use_root_partition=#{use_root_partition} save_root_partition=#{save_root_partition} rw"
+    pv_device = "#{self.pv_device}" if @hash["pv_device"]? != nil
+    return "#{common_params} local use_root_partition=#{use_root_partition} save_root_partition=#{save_root_partition} pv_device=#{pv_device} rw"
   end
 
   private def kernel_custom_params
