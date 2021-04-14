32767e4166300423cc9719cec3a746ccbcd1cf2d
Cao Xueliang <caoxl78320@163.com>
2021-04-14 09:58:30 +0800

    sbin: add config account yaml script
    delimiter/delimiter.cr needs it to create delimiter account.
    
    Signed-off-by: Cao Xueliang <caoxl78320@163.com>

---
+++ sbin/config_account_yaml.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# SPDX-License-Identifier: MulanPSL-2.0+
+# Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
+# frozen_string_literal: true
+
+require_relative "../lib/config_account"
+
+config_yaml ARGV[0]
