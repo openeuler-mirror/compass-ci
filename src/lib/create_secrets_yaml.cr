f7b6126475a153d7dd92f5ac22647b4b21a32596
Cao Xueliang <caoxl78320@163.com>
2021-05-31 19:44:44 +0800

    src/lib: add create_secrets_yaml to lib
    Signed-off-by: Cao Xueliang <caoxl78320@163.com>

---
+++ src/lib/create_secrets_yaml.cr
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: MulanPSL-2.0+
+# Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
+
+require "yaml"
+
+def create_secrets_yaml(type)
+  %x(#{ENV["CCI_SRC"]}/sbin/config_account_yaml.rb #{type})
+  account = YAML.parse(File.read("#{ENV["HOME"]}/.config/compass-ci/defaults/account.yaml"))
+  lab = YAML.parse(File.read("#{ENV["HOME"]}/.config/compass-ci/include/lab/#{account["lab"]}.yaml"))
+  secrets = {"secrets" => lab}
+  File.open("#{ENV["HOME"]}/.config/compass-ci/defaults/secrets.yaml", "w") { |f| YAML.dump(secrets, f) }
+end
