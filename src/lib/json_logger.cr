d2325a78c15d1e984284065aa21876d5b4f25617
Wu Zhende <wuzhende666@163.com>
2021-06-02 10:26:27 +0800

    lib/json_logger.cr: add some fields
    Print some important fields through the env transfer.
    Like "testbox" "job_id" and "job_state".
    JSON logs can be transferred from the env and merged to the logs be
    printed.
    
    Signed-off-by: Wu Zhende <wuzhende666@163.com>

--- src/lib/json_logger.cr
+++ src/lib/json_logger.cr
@@ -10,7 +10,7 @@ add_context_storage_type(Time::Span)
 
 class JSONLogger < Logger
   def initialize(logdev = STDOUT, formatter = my_formatter, @env = nil)
-    @env_info = Hash(String, String | Int32 | Float64).new
+    @env_info = Hash(String, String | Int32 | Float64 | JSON::Any).new
     super(logdev, formatter: formatter)
   end
 
@@ -42,8 +42,20 @@ class JSONLogger < Logger
     @env_info["status_code"] = env.response.status_code
     @env_info["method"] = env.request.method
     @env_info["resource"] = env.request.resource
-    
+
+    @env_info["testbox"] = env.get?("testbox").to_s if env.get?("testbox")
+    @env_info["job_id"] = env.get?("job_id").to_s if env.get?("job_id")
+    @env_info["job_state"] = env.get?("job_state").to_s if env.get?("job_state")
+
     set_elapsed(env)
+    merge_env_log(env)
+  end
+
+  private def merge_env_log(env)
+    return unless log = env.get?("log")
+
+    log = JSON.parse(log.to_s).as_h
+    @env_info.any_merge!(log)
   end
 
   private def set_elapsed(env : HTTP::Server::Context)
