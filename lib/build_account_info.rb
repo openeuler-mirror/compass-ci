cefe76bd85170590c8e96fad1c0d2d8cf6cac6cd
Luan Shengde <shdluan@163.com>
2021-06-23 19:13:24 +0800

    lib/build_account_info: optimize and fix bug
    optimize:
      add search_account_info:
        enable user to search if my_info exists before build it
    
    fix bug:
      when the existing my_info already has key my_ssh_pubkey, when do the
      build_my_info, it will clear the existing data for my_ssh_pubkey,
    
      resolve:
        merge the my_ssh_pubkey from account to info_es instead of replace
        it.
    
    Signed-off-by: Luan Shengde <shdluan@163.com>

--- lib/build_account_info.rb
+++ lib/build_account_info.rb
@@ -6,12 +6,26 @@
 require_relative './es_client'
 require_relative './build_my_info_client'
 
-def build_account_info(option)
+def search_account_info(option)
   build_my_info = BuildMyInfo.new(option['my_email'])
   info_es = build_my_info.search_my_info
+
+  [info_es, build_my_info]
+end
+
+def build_account_info(option)
+  info_es, build_my_info = search_account_info(option)
+
+  my_ssh_pubkey = option.delete('my_ssh_pubkey') unless option['my_ssh_pubkey'].nil?
+
   info_es.update option
+
   info_es['my_token'] = %x(uuidgen).chomp if info_es['my_token'].nil?
-  info_es['my_ssh_pubkey'] = []
+  info_es['my_ssh_pubkey'] = [] if info_es['my_ssh_pubkey'].nil?
+  unless my_ssh_pubkey.empty?
+    info_es['my_ssh_pubkey'] = info_es['my_ssh_pubkey'] + my_ssh_pubkey
+    info_es['my_ssh_pubkey'].uniq!
+  end
 
   build_my_info.config_my_info(info_es)
 end
