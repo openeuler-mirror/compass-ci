9016b3e6e3b4c82a8472b5ea958a63335dcc681b
Cao Xueliang <caoxl78320@163.com>
2021-07-01 11:45:22 +0800

    lib: improve code readability
    Signed-off-by: Cao Xueliang <caoxl78320@163.com>

--- lib/compare_error_messages.rb
+++ lib/compare_error_messages.rb
@@ -12,29 +12,30 @@ module CEM
   extend self
 
   # previous_job_id's error_ids include error_id
-  # If later_job_id's output include error filename that extract from error_id,
-  # the later_job_id is credible.
-  def credible?(previous_job_id, later_job_id, error_id)
+  # If lttr_job_id's output include error filename that extract from error_id,
+  # the lttr_job_id is credible.
+  def credible?(prev_job_id, lttr_job_id, error_id)
     es = ESQuery.new
-    later_es_result = es.query_by_id(later_job_id)
+    lttr_es_result = es.query_by_id(lttr_job_id)
 
-    return true if later_es_result['job_state'] == 'finished'
+    return true if lttr_es_result['job_state'] == 'finished'
 
-    previous_result_file = File.join('/srv', es.query_by_id(previous_job_id)['result_root'], es.query_by_id(previous_job_id)['suite'])
-    later_result_file = File.join('/srv', later_es_result['result_root'], later_es_result['suite'])
+    prev_es_result = es.query_by_id(prev_job_id)
+    prev_result_file = File.join('/srv', prev_es_result['result_root'], prev_es_result['suite'])
+    lttr_result_file = File.join('/srv', lttr_es_result['result_root'], lttr_es_result['suite'])
 
-    filenames_check = filenames_check(previous_result_file, later_result_file, error_id)
+    check_result = filenames_check(prev_result_file, lttr_result_file, error_id)
 
-    return false if filenames_check.empty? || filenames_check.value?(false)
+    return false if check_result.empty? || check_result.value?(false)
 
     return true
   end
 
-  def filenames_check(previous_result_file, later_result_file, error_id)
+  def filenames_check(prev_result_file, lttr_result_file, error_id)
     filenames = Set.new
-    filenames_check = Hash.new { |h, k| h[k] = false }
+    ret = Hash.new { |h, k| h[k] = false }
 
-    error_lines = ErrorMessages.new(previous_result_file).obtain_error_messages_by_error_id(error_id, true)
+    error_lines = ErrorMessages.new(prev_result_file).obtain_error_messages_by_error_id(error_id, true)
     error_lines.each do |error_line|
       # "src/ssl_sock.c:1454:104: warning: unused parameter 'al' [-Wunused-parameter]" => "src/ssl_sock"
       # "/tmp/lkp/bart-git/src/bart/src/num/lapack.c:20:10: fatal error: lapacke.h: No such file or directory"
@@ -42,14 +43,15 @@ module CEM
       filenames << $1.chomp(File.extname($1)) if error_line =~ /(.*)(:\d+){2}: ((fatal )?error|warning):/
     end
 
-    File.open(later_result_file).each_line do |line|
+    File.open(lttr_result_file).each_line do |line|
       filenames.each do |filename|
-        filenames_check[filename]
-        filenames_check[filename] = true if line.include?(filename)
+        # default init filename value to false
+        ret[filename]
+        ret[filename] = true if line.include?(filename)
       end
     end
 
-    return filenames_check
+    return ret
   end
 
   def get_compare_result(previous_job_id, later_job_id)
