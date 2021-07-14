4ab3fee1634e174dbaca7f7ecf1983f7e396708e
Cao Xueliang <caoxl78320@163.com>
2021-07-14 16:50:26 +0800

    plugins: add finally plugin class
    if no plugin add the job to queue,
    the finally plugin will add the job to ready queue.
    
    Signed-off-by: Cao Xueliang <caoxl78320@163.com>

---
+++ src/scheduler/plugins/finally.cr
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: MulanPSL-2.0+
+# Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
+require "./plugins_common"
+
+class Finally < PluginsCommon
+  def handle_job(job)
+    return if job.has_key?("added")
+    save_job2es(job)
+    save_job2etcd(job)
+    add_job2queue(job)
+  end
+
+  def add_job2queue(job)
+    job["added"] = ["finally"]
+    key = "sched/ready/#{job.queue}/#{job.subqueue}/#{job.id}"
+    value = {"id" => JSON::Any.new("#{job.id}")}
+
+    response = @etcd.put(key, value.to_json)
+    raise "add the job to queue failed: id #{job.id}, queue #{key}" unless response
+  end
+end
