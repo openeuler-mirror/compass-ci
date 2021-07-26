2b3b5a41394bc6b9a10f8c0f414e5e437261aac0
Cui Yili <2268260388@qq.com>
2021-07-26 10:48:30 +0800

    generate_conserver.rb: add more configuration files
    [why]
    /etc/mac2host and /etc/servers.info is more accurate.
    
    Signed-off-by: Cui Yili <2268260388@qq.com>

--- container/conserver/generate_conserver.rb
+++ container/conserver/generate_conserver.rb
@@ -9,12 +9,12 @@ lab = 'z9'
 lab = 'crystal' if ENV['HOSTNAME'].include?("crystal")
 CCI_REPOS = ENV['CCI_REPOS'] || '/c'
 
-def generate_conserver(lab)
-  return unless File.exist?('conserver-head.cf')
+exit 1 unless File.exist?('conserver-head.cf')
+FileUtils.cp('conserver-head.cf', 'conserver.cf')
 
-  FileUtils.cp('conserver-head.cf', 'conserver.cf')
-  
+def generate_conserver_by_lab(lab)
   host_dir = "#{CCI_REPOS}/lab-#{lab}/hosts/"
+  puts host_dir
   unless Dir.exist?(host_dir)
     puts "please check whether the #{host_dir} directory exists "
     return
@@ -33,4 +33,46 @@ def generate_conserver(lab)
   end
 end
 
-generate_conserver(lab)
+def generate_conserver_by_cfg(host_file, server_file)
+  hosts = Hash.new
+  servers = Hash.new
+
+  host_ff = File.open(host_file)
+  host_ff.each_line do |line|
+    # line e.g.: pe-c cc-05-77-fe-cf-86 taishan200-2280-2s64p-256g--a143
+    if line =~ %r|(p[^ ]*) ([^ ]*) (taishan.*)|
+        num = $1.chomp
+        name = $3.chomp
+        hosts[num] = name
+    end
+  end
+
+  server_ff = %x(awk '{print $1" "$2}' #{server_file})
+  server_ff.each_line do |line|
+    # line e.g.: 9.3.14.12 pe-c
+    if line =~ %r|(^9[^ ]*) (p[^ ]*)|
+        ip = $1.strip
+        num = $2.strip
+        servers[num] = ip
+    end
+  end
+
+  hosts.each do |num, name|
+    console = <<~HEREDOC
+    console #{name} {
+      exec /usr/local/bin/ipmi-sol #{servers[num]};
+    }
+    HEREDOC
+    File.open('conserver.cf', 'a') { |f| f << console }
+  end
+
+end
+
+host_file = "/etc/mac2host"
+server_file = "/etc/servers.info"
+
+if File.exist?(host_file) and File.exist?(server_file)
+  generate_conserver_by_cfg(host_file, server_file)
+else
+  generate_conserver_by_lab(lab)
+end
