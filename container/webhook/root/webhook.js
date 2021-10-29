c42f5349ccb08ef37825c16ae1462706fa93e545
Wang Lin <wanglin199@huawei.com>
2021-10-29 16:52:50 +0800

    container/webhook: add api to handle pr sent from gitee
    We want to monitor 7000+ packages under src-openeuler,
    after submitting pr, test rpmbuild based on this pr code.
    
    First,configure webhook for src-openeuler, including the api url and watch event.
    When there is a newly submitted pr in the src-openeuler, gitee will send a post request including webhook data,
    then parse the payload sent by gitee and send message to MQ.
    And this process only deals with pr submit, does not deal with pr merges
    and closes.

--- container/webhook/root/webhook.js
+++ container/webhook/root/webhook.js
@@ -9,7 +9,26 @@ handler.on('error', function(err){
 
 handler.on('push', function(event){
 	console.log(event.payload.repository.url)
-	spawn('ruby', ['push_hook.rb', event.payload.repository.url])
+	spawn('ruby', ['/js/push_hook.rb', event.payload.repository.url])
+})
+
+handler.on('Merge Request Hook', function(event){
+	if(event.payload.action != "open"){
+		return
+	}
+	var msg = {
+		"new_refs" : {
+			"heads" : {
+				"master" : event.payload.pull_request.base.sha
+			}
+		},
+		"url" : event.payload.pull_request.base.repo.url,
+		"submit_command" : {
+			"pr_merge_reference_name" : event.payload.pull_request.merge_reference_name
+		}
+	}
+
+	spawn('ruby', ['/js/pr_hook.rb', JSON.stringify(msg)])
 })
 
 http.createServer(function(req, res){
