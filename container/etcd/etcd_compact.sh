7d076e9fcb8a8bd97dc5c602dbc2643bf4bd6cd8
Wu Fengguang <wfg@mail.ustc.edu.cn>
2023-02-09 18:54:51 +0800

    etcd: defrag every week
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- container/etcd/etcd_compact.sh
+++ container/etcd/etcd_compact.sh
@@ -3,4 +3,5 @@
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
 
 rev=$(etcdctl endpoint status --write-out="json"|egrep -o '"revision":[0-9]*'|egrep -o '[0-9].*')
-etcdctl compact $rev && etcdctl --command-timeout=30s defrag
+etcdctl compact $rev
+[ $(date +%A) = Sunday ] && etcdctl --command-timeout=30s defrag
