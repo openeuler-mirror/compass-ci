d1e7921abd4b0b79a21262706cad76541840747d
Wu Fengguang <wfg@mail.ustc.edu.cn>
2023-02-09 19:02:33 +0800

    etcd: keep at least 10k history
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- container/etcd/etcd_compact.sh
+++ container/etcd/etcd_compact.sh
@@ -3,5 +3,11 @@
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
 
 rev=$(etcdctl endpoint status --write-out="json"|egrep -o '"revision":[0-9]*'|egrep -o '[0-9].*')
+
+KEEP=10000 # keep 10k revision history
+[ $rev -gt $KEEP ] || exit 0
+rev=$((rev-$KEEP))
+
 etcdctl compact $rev
+
 [ $(date +%A) = Sunday ] && etcdctl --command-timeout=30s defrag
