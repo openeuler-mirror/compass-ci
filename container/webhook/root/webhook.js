7d85fb389c58f821531e4ac80a309080bc5a7e3e
t3190102779 <zsyxzu@163.com>
2023-09-19 22:01:18 +0800

    update webhook.js

--- container/webhook/root/webhook.js
+++ container/webhook/root/webhook.js
@@ -50,6 +50,31 @@ handler.on('Merge Request Hook', function(event){
 	}
 })
 
+handler.on('Note Hook', function(event){
+	if(event.payload.action == "comment" && event.payload.comment.body == "/retest"){
+		var msg = {
+            "new_refs" : {
+                "heads" : {
+                    "master" : event.payload.pull_request.head.sha
+                }
+            },
+            "branch" :  event.payload.pull_request.base.ref,
+            "url" : event.payload.pull_request.base.repo.url,
+            "submit_command" : {
+                "pr_merge_reference_name" : event.payload.pull_request.merge_reference_name
+            }
+        }
+        console.log(msg)
+    
+        if(event.payload.pull_request.base.repo.url.startsWith("https://gitee.com/src-oepkgs/")){
+            spawn('ruby', ['/js/src_oepkgs_pr_hook.rb', JSON.stringify(msg)])
+        } else {
+            spawn('ruby', ['/js/pr_hook.rb', JSON.stringify(msg)])
+        }
+	}
+	
+})
+
 http.createServer(function(req, res){
 	handler(req, res, function(err){
 		res.statusCode = 404
