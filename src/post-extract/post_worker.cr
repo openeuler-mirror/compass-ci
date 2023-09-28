89d6649f5189ce4908b2f5e19dbeb47d89261d26
Ethan-Zhang <ethanzhang55@outlook.com>
2023-09-28 15:24:16 +0800

    Fix: change queue prefix

--- src/post-extract/post_worker.cr
+++ src/post-extract/post_worker.cr
@@ -44,7 +44,7 @@ class PostWorker
     workflow_exec_id = job["workflow_exec_id"]?.to_s
     return if workflow_exec_id.nil? || workflow_exec_id.empty?
 
-    post_extract_queue = "/events/post_extract/#{workflow_exec_id}/#{job_id}"
+    post_extract_queue = "/post_extract/#{workflow_exec_id}/#{job_id}"
     @etcd.put(post_extract_queue, job.to_json)
     @log.info("post-extract send workflow event, id:#{job_id}")
   end
