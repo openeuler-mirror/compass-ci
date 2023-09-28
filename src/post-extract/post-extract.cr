db1553713938c049865bd1c1aaa88e825c406404
Ethan-Zhang <ethanzhang55@outlook.com>
2023-09-28 14:46:38 +0800

    Feat: add workflow event handling in post-extract

--- src/post-extract/post-extract.cr
+++ src/post-extract/post-extract.cr
@@ -3,7 +3,7 @@
 
 
 require "../lib/etcd_client"
-require "./mail_worker"
+require "./post_worker"
 
 module PostExtract
   @@ec = EtcdClient.new
@@ -66,7 +66,7 @@ module PostExtract
   def self.handle_events(channel)
     while true
       key = channel.receive
-      spawn { MailWorker.new.handle(key, channel) }
+      spawn { PostWorker.new.handle(key, channel) }
       Fiber.yield
     end
   end
