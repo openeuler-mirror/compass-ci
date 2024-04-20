f5b897c98951757381cc50fec3c36decaf3d503d
Wu Fengguang <wfg@mail.ustc.edu.cn>
2024-04-20 17:25:37 +0800

    job2sh: support namespace hw and services
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- src/scheduler/create_job_cpio.cr
+++ src/scheduler/create_job_cpio.cr
@@ -73,6 +73,12 @@ class Sched
     script_lines << "\n"
 
     job_content.each { |key, val| parse_one(script_lines, key, val) }
+    if job_content.includes? "hw"
+      job_content["hw"].as_h.each { |key, val| parse_one(script_lines, key, val) }
+    end
+    if job_content.includes? "services"
+      job_content["services"].as_h.each { |key, val| parse_one(script_lines, key, val) }
+    end
 
     script_lines << "}\n"
 
