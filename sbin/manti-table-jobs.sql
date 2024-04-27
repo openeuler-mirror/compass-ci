6bfdd45ea176b79889899bed576358ca25974bbf
Wu Fengguang <wfg@mail.ustc.edu.cn>
2024-04-27 19:38:27 +0800

    jobs: update schema order
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- sbin/manti-table-jobs.sql
+++ sbin/manti-table-jobs.sql
@@ -4,6 +4,14 @@
 # /c/manticoresearch/manual/Creating_a_table/NLP_and_tokenization/Low-level_tokenization.md
 # https://www.ime.usp.br/~pf/algorithms/appendices/ascii.html
 
+# Actions during the job lifecycle:
+# - initial INSERT: submit initial job spec
+# - stage UPDATE: when job is consumed and run, will update few 'rowwise' stage/time fields for many times
+# - final REPLACE: add lots of errid/stats/result
+# - data SEARCH: batch queries from cli/web ui, compare, bisect, etc.
+
+# the 'rowwise' fields are in order of mutable, in hope of improving efficiency: int/json/string 
+
 CREATE TABLE jobs(
 	id		bigint,
 	submit_id	bigint,
@@ -32,10 +40,10 @@ CREATE TABLE jobs(
 
 	lab		string,
 	tbox_group	string,
-	testbox		string engine='rowwise',
 
-	job_stage	string engine='rowwise',
-	job_health	string engine='rowwise',
+	pp		json,
+	ss		json,
+	hw		json,
 
 	boot_time	bigint engine='rowwise',
 	finish_time	bigint engine='rowwise',
@@ -44,16 +52,20 @@ CREATE TABLE jobs(
 	boot_seconds	int engine='rowwise',
 	run_seconds	int engine='rowwise',
 
-	pp		json engine='rowwise',
-	ss		json engine='rowwise',
-	hw		json engine='rowwise',
 	jj		json engine='rowwise',
+
+	testbox		string engine='rowwise',
+
+	job_stage	string engine='rowwise',
+	job_health	string engine='rowwise',
+
+	mutable_vars	string stored engine='rowwise',
 
 	errid		string,
 	stats		string stored,
 
-	mutable_vars	string stored engine='rowwise',
 	other_data	string stored,
+
 	full_text_words text indexed,
 ) engine='columnar' charset_table='U+0021..U+007E';
 
