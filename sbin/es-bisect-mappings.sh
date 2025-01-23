ea87fbb1f1c233a80924616d8a79244a865bb293
jacknichao <chaoni@zju.edu.cn>
2025-01-23 13:48:08 +0800

    Fix for review

--- sbin/es-bisect-mappings.sh
+++ sbin/es-bisect-mappings.sh
@@ -11,78 +11,60 @@ load_cci_defaults
 if ! [ -x "$(command -v curl)" ]
 then
 	echo "curl not exists, try to install."
-	if [ -x "$(command -v apk)" ]
-	then
-		apk add install -y curl
-	elif [ -x "$(command -v yum)" ]
-	then
-		yum install -y curl
-	elif [ -x "$(command -v apt-get)" ]
-	then
-		apt-get install -y curl
-	fi
+	exit
 else
 	echo "curl has install."
 fi
 
-# Determine whether curl is installed successfully
-if [ $? -ne 0 ]
-then
-	echo "curl install failed, exit."
-	exit
-fi
+# Define a string for bisect_task properties
+PROPERTIES="{
+        		\"id\": {\"type\": \"keyword\"},
+        		\"bad_job_id\": {\"type\": \"keyword\"},
+        		\"error_id\": {\"type\": \"keyword\"},
+			\"bisect_status\": {\"type\": \"keyword\"},
+        		\"repo\": {\"type\": \"keyword\"},
+        		\"bad_commit\": {\"type\": \"keyword\"},
+        		\"work_dir\": {\"type\": \"keyword\"},
+        		\"bisect_error\": {\"type\": \"keyword\"},
+        		\"all_errors\": {\"type\": \"keyword\"},
+        		\"upstream_url\": {\"type\": \"keyword\"},
+        		\"pkgbuild_repo\": {\"type\": \"keyword\"},
+        		\"first_bad_commit_result_root\": {\"type\": \"keyword\"}
+        }"
+
+INDEX_NAME="bisect_task"
 
 # Determine whether bisect index has created
-status_code=$(curl -sSIL -u "${ES_SUPER_USER}:${ES_SUPER_PASSWORD}" -w "%{http_code}\n" -o /dev/null http://${ES_HOST}:9200/bisect-task)
+status_code=$(curl -sSIL -u "${ES_SUPER_USER}:${ES_SUPER_PASSWORD}" -w "%{http_code}\n" -o /dev/null http://${ES_HOST}:9200/${INDEX_NAME})
 
 if [ $status_code -eq 200 ]
 then
 	echo "bisect_task index has create, exit."
-	curl -sSH 'Content-Type: Application/json' -XPUT "http://${ES_HOST}:9200/bisect_task/_mapping" -u "${ES_SUPER_USER}:${ES_SUPER_PASSWORD}" -d '{
-	"properties": {
-		"id": {"type": "keyword"},
-		"bad_job_id": {"type": "keyword"},
-		"error_id": {"type": "keyword"},
-		"bisect_status": {"type": "keyword"},
-		"repo": {"type": "keyword"},
-		"bad_commit": {"type": "keyword"},
-		"work_dir": {"type": "keyword"},
-		"bisect_error": {"type": "keyword"},
-		"all_errors": {"type": "keyword"},
-		"upstream_url": {"type": "keyword"},
-		"pkgbuild_repo": {"type": "keyword"},
-		"first_bad_commit_result_root": {"type": "keyword"}
-	}}'
+	curl -sSH 'Content-Type: Application/json' \
+	     -XPUT "http://${ES_HOST}:9200/${INDEX_NAME}/_mapping" \
+	     -u "${ES_SUPER_USER}:${ES_SUPER_PASSWORD}" \
+	     -d "{
+		\"properties\": $PROPERTIES
+		}"
 else
 	echo "bisect_task index not exists, begin create index."
-	curl -sSH 'Content-Type: Application/json' -XPUT 'http://localhost:9200/bisect_task' -u "${ES_USER}:${ES_PASSWORD}" -d '{
-	    "settings": {
-        	"number_of_shards": 1,
-        	"number_of_replicas": 1
+	curl -sSH 'Content-Type: Application/json' \
+	     -XPUT "http://localhost:9200/${INDEX_NAME}" \
+	     -u "${ES_USER}:${ES_PASSWORD}" \
+	     -d "{
+	    	\"settings\": {
+        	\"number_of_shards\": 1,
+        	\"number_of_replicas\": 1
     		},
-    	"mappings": {
-        "properties": {
-		"id": {"type": "keyword"},
-		"bad_job_id": {"type": "keyword"},
-		"errid": {"type": "keyword"},
-		"bisect_status": {"type": "keyword"},
-		"repo": {"type": "keyword"},
-		"bad_commit": {"type": "keyword"},
-		"work_dir": {"type": "keyword"},
-		"bisect_error": {"type": "keyword"},
-		"all_errors": {"type": "keyword"},
-		"upstream_url": {"type": "keyword"},
-		"pkgbuild_repo": {"type": "keyword"},
-		"first_bad_commit_result_root": {"type": "keyword"}
-        	}
-		}
-	}'
+		\"mappings\": {
+		\"properties\": $PROPERTIES
+		}}"
 	if [ $? -ne 0 ]
 	  then
 		  echo "create bisect_task index failed."
 	  else
 		  echo "set index.mapping.total_fields.limit: 10000"
-		  curl -sS -XPUT "${ES_HOST}":9200/bisect_task/_settings -u "${ES_SUPER_USER}:${ES_SUPER_PASSWORD}" -H 'Content-Type: application/json' \
+		  curl -sS -XPUT "${ES_HOST}":9200/${INDEX_NAME}/_settings -u "${ES_SUPER_USER}:${ES_SUPER_PASSWORD}" -H 'Content-Type: application/json' \
 		       -d '{"index.mapping.total_fields.limit": 10000}'
 	fi
 fi
