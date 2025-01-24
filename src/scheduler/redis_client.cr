7799a540ea5674d18b4b82262f9ae01e20059212
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-01-24 17:44:02 +0800

    redis: add in_mem_hash alternative
    - Added @in_mem_hash: A Hash(String, Hash(String, String)) to simulate Redis operations in memory.
    - Conditional Logic: All methods now check Sched.options.hash_redis to determine whether to use @client (Redis) or @in_mem_hash (in-memory hash).
    - Ignored expire: The expire method is ignored for the in-memory hash since it doesn't have a simple implementation.
    - Simulated Redis Operations: Methods like hash_set, hash_get, set, get, etc., now work with @in_mem_hash when hash_redis is false.
    
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- src/scheduler/redis_client.cr
+++ src/scheduler/redis_client.cr
@@ -13,18 +13,26 @@ class RedisClient
   class_property :client
   @@size = 25
 
+  # In-memory hash to simulate Redis when has_redis is false
+  @in_mem_hash : Hash(String, Hash(String, String)) = Hash(String, Hash(String, String)).new
+
   def self.instance
     Singleton::Of(self).instance
   end
 
   def initialize
-    host =  Sched.options.redis_host
-    port =  Sched.options.redis_port
+    host = Sched.options.redis_host
+    port = Sched.options.redis_port
     passwd = Sched.options.redis_passwd
-    if Sched.options.redis_is_cluster
-      @client = Redis::Cluster.new(URI.parse("redis://:#{passwd}@#{host}:#{port}"))
+
+    if Sched.options.has_redis
+      if Sched.options.redis_is_cluster
+        @client = Redis::Cluster.new(URI.parse("redis://:#{passwd}@#{host}:#{port}"))
+      else
+        @client = Redis::Client.new(URI.parse("redis://:#{URI.encode_www_form(passwd)}@#{host}:#{port}"))
+      end
     else
-      @client = Redis::Client.new(URI.parse("redis://:#{URI.encode_www_form(passwd)}@#{host}:#{port}"))
+      @client = nil # No Redis client when has_redis is false
     end
   end
 
@@ -32,61 +40,108 @@ class RedisClient
     @@size = pool_size
   end
 
-  def keys(pattern)
-    @client.keys(pattern)
+  def client
+    @client.not_nil!
   end
 
   def scan_each(pattern)
-    keys = [] of String
-    if Sched.options.redis_is_cluster
-      @client.scan_each(match: pattern) do |key|
-        keys << key
+    if Sched.options.has_redis
+      keys = [] of String
+      if Sched.options.redis_is_cluster
+        self.client.scan_each(match: pattern) do |key|
+          keys << key
+        end
+      else
+        return self.client.keys(pattern)
       end
+      keys
     else
-      return @client.keys(pattern)
+      # Simulate keys matching in in-memory hash
+      prefix = pattern.chomp('*')
+      @in_mem_hash.keys.select { |key| key.starts_with?(prefix) }
     end
-
-    keys
   end
 
   def hash_set(key : String, field, value)
-    @client.hset(key, field.to_s, value.to_s)
+    if Sched.options.has_redis
+      self.client.hset(key, field.to_s, value.to_s)
+    else
+      @in_mem_hash[key] ||= Hash(String, String).new
+      @in_mem_hash[key][field.to_s] = value.to_s
+    end
   end
 
   def hash_get(key : String, field)
-    "#{@client.hget(key, field.to_s)}"
+    if Sched.options.has_redis
+      self.client.hget(key, field.to_s).to_s
+    else
+      @in_mem_hash[key]?.try(&.[](field.to_s)) || ""
+    end
   end
 
   def hash_del(key : String, field)
-    return nil
+    if Sched.options.has_redis
+      self.client.hdel(key, field.to_s)
+    else
+      @in_mem_hash[key]?.try(&.delete(field.to_s))
+    end
   end
 
   def set(key : String, value)
-    @client.set(key, value.to_s)
+    if Sched.options.has_redis
+      self.client.set(key, value.to_s)
+    else
+      @in_mem_hash[key] = {"" => value.to_s}
+    end
   end
 
   def get(key : String)
-    @client.get(key)
+    if Sched.options.has_redis
+      self.client.get(key)
+    else
+      @in_mem_hash[key]?.try(&.[](""))
+    end
   end
 
   def del(key : String)
-    @client.del(key)
+    if Sched.options.has_redis
+      self.client.del(key)
+    else
+      @in_mem_hash.delete(key)
+    end
   end
 
   def expire(key : String, duration)
-    @client.expire(key, duration)
+    if Sched.options.has_redis
+      self.client.expire(key, duration)
+    else
+      # Ignore expire for in-memory hash
+    end
   end
 
   def get_job(job_id : String)
-    job_hash = @client.hget("sched/id2job", job_id)
-    if !job_hash
-      raise "Get job (id = #{job_id}) from redis failed."
+    if Sched.options.has_redis
+      job_hash = self.client.hget("sched/id2job", job_id)
+      if !job_hash
+        raise "Get job (id = #{job_id}) from redis failed."
+      end
+      Job.new(JSON.parse(job_hash).as_h, job_id)
+    else
+      job_hash = @in_mem_hash["sched/id2job"]?.try(&.[](job_id))
+      if !job_hash
+        raise "Get job (id = #{job_id}) from in-memory hash failed."
+      end
+      Job.new(JSON.parse(job_hash).as_h, job_id)
     end
-    Job.new(JSON.parse(job_hash).as_h, job_id)
   end
 
   def update_wtmp(testbox : String, wtmp_hash : Hash)
-    @client.hset("sched/tbox_wtmp", testbox, wtmp_hash.to_json)
+    if Sched.options.has_redis
+      self.client.hset("sched/tbox_wtmp", testbox, wtmp_hash.to_json)
+    else
+      @in_mem_hash["sched/tbox_wtmp"] ||= Hash(String, String).new
+      @in_mem_hash["sched/tbox_wtmp"][testbox] = wtmp_hash.to_json
+    end
   end
 
   def update_job(job_content : JSON::Any | Hash)
@@ -103,6 +158,10 @@ class RedisClient
   end
 
   def remove_finished_job(job_id : String)
-    @client.hdel("sched/id2job", job_id)
+    if Sched.options.has_redis
+      self.client.hdel("sched/id2job", job_id)
+    else
+      @in_mem_hash["sched/id2job"]?.try(&.delete(job_id))
+    end
   end
 end
