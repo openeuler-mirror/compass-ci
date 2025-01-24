c7335b58cfe9be3823499e7baf3459f610bc71db
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-01-24 16:13:30 +0800

    scheduler: support config file
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- src/lib/sched.cr
+++ src/lib/sched.cr
@@ -42,10 +42,34 @@ require "../scheduler/plugins/finally"
 require "../scheduler/plugins/cluster"
 require "../scheduler/heart_beat"
 
+# configuration options
+SCHEDULER_CONFIG_FILE = "/etc/compass-ci/scheduler/config.yaml" # Default config file
+struct SchedOptions
+  include YAML::Serializable
+
+  property has_redis : Bool = true
+  property has_etcd : Bool = true
+  property has_es : Bool = true
+  property has_manticore : Bool = false
+
+  property sched_port : Int32 = 3000
+
+  def initialize
+  end
+
+  # call in the end, after SchedOptions.from_yaml()
+  def load_env
+    sched_port = ENV["sched_port"].to_i32 if ENV["sched_port"]?
+  end
+
+end
+
 class Sched
   property es
   property redis
   property block_helper
+
+  class_property options = SchedOptions.new
 
   @@block_helper = BlockHelper.new
 
--- src/scheduler.cr
+++ src/scheduler.cr
@@ -10,17 +10,81 @@ require "./lib/queue"
 require "./lib/subqueue"
 require "./lib/init_ready_queues"
 
-module Scheduler
-  log = JSONLogger.new
+require "kemal"
+require "option_parser"
+require "yaml" # For parsing the config file
 
-  begin
+module Scheduler
+
+  # Initialize the logger
+  LOG = JSONLogger.new
+
+  # Parse command-line options
+  def self.parse_options
+    config_file = SCHEDULER_CONFIG_FILE
+    OptionParser.parse do |parser|
+      parser.banner = "Usage: scheduler [arguments]"
+
+      parser.on "-c CONFIG", "--config=CONFIG", "Path to the configuration file" do |config|
+        config_file = config
+      end
+
+      parser.on "-h", "--help", "Show help" do
+        puts parser
+        exit
+      end
+
+      parser.invalid_option do |flag|
+        STDERR.puts "ERROR: #{flag} is not a valid option."
+        STDERR.puts parser
+        exit(1)
+      end
+    end
+
+    load_config(config_file)
+  end
+
+  # Load the configuration file into a SchedOptions struct
+  def self.load_config(config_file : String)
+    if File.exists?(config_file)
+      Sched.options = SchedOptions.from_yaml(File.read(config_file))
+      Sched.options.load_env # ENV vars can override config options
+    else
+      LOG.warn("Config file #{config_file} not found. Using default options.")
+    end
+  rescue e
+    LOG.error("Failed to load config file: #{e}")
+    exit(1)
+  end
+
+  def self.initialize_scheduler
     create_secrets_yaml("scheduler")
     do_local_pack
+  end
+
+  # Start background tasks
+  def self.start_background_tasks
     spawn Queue.instance.timing_refresh_from_etcd
     spawn Subqueue.instance.timing_refresh_from_es
     spawn InitReadyQueues.instance.loop_init
+  end
+
+  # Start the Kemal server using the configuration
+  def self.start_kemal_server
     Kemal.run(ENV["NODE_PORT"].to_i32)
+  end
+
+  # Main entry point for the scheduler
+  def self.run
+    parse_options
+
+    initialize_scheduler
+    start_background_tasks
+    start_kemal_server
   rescue e
-    log.error(e)
+    LOG.error(e)
   end
 end
+
+# Run the scheduler
+Scheduler.run
