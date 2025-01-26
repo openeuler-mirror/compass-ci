5698125bea94fd4d87f54fcf7cd324cf71c29182
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-01-26 20:16:25 +0800

    simplify and fixup sbin/manti-table-jobs.sql
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- sbin/manti-table-jobs.sql
+++ sbin/manti-table-jobs.sql
@@ -1,6 +1,4 @@
 
-# curl -sX POST http://localhost:9308/cli -d "$(<manti-table-jobs.sql)"
-
 # https://manticoresearch.com/blog/basics-of-manticore-indexes/
 # https://manual.manticoresearch.com/Updating_table_schema_and_settings
 # /c/manticoresearch/manual/Creating_a_table/NLP_and_tokenization/Low-level_tokenization.md
@@ -8,7 +6,7 @@
 
 # Actions during the job lifecycle:
 # - initial INSERT: submit initial job spec
-# - stage UPDATE: when job is consumed and run, will update few 'rowwise' stage/time fields for many times
+# - stage REPLACE: when job is consumed and run
 # - final REPLACE: add lots of errid/stats/result
 # - data SEARCH: batch queries from cli/web ui, compare, bisect, etc.
 
@@ -16,59 +14,17 @@
 
 CREATE TABLE jobs(
 	id		bigint,
-	submit_id	bigint,
+
 	submit_time	bigint,
-	submit_date	string,
+	boot_time	bigint,
+	running_time	bigint,
+	finish_time	bigint,
 
-	my_account	string,
-	my_name		string,
+	boot_seconds	int,
+	run_seconds	int,
 
-	suite		string,
-	category	string,
+	j		json,
+	errid		text,
+	full_text_kv	text indexed
 
-	os		string,
-	os_version	string,
-	os_arch		string,
-
-	osv		string,
-	os_mount	string,
-	rootfs		string,
-
-	pp_params_md5	string,
-	all_params_md5	string,
-
-	queue		string,
-
-	lab		string,
-	tbox_group	string,
-
-	pp		json,
-	ss		json,
-	hw		json,
-
-	boot_time	bigint engine='rowwise',
-	running_time	bigint engine='rowwise',
-	finish_time	bigint engine='rowwise',
-	time		bigint engine='rowwise',
-
-	boot_seconds	int engine='rowwise',
-	run_seconds	int engine='rowwise',
-
-	jj		json engine='rowwise',
-
-	testbox		string engine='rowwise',
-
-	job_stage	string engine='rowwise',
-	job_health	string engine='rowwise',
-
-	mutable_vars	string stored engine='rowwise',
-
-	errid		string,
-	stats		string stored,
-
-	other_data	string stored,
-
-	full_text_words text indexed,
-) engine='columnar' charset_table='U+0021..U+007E';
-
-desc jobs;
+) engine='columnar' charset_table='U+0021..U+007E'
