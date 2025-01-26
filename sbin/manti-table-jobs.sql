ab4d0d5c00451ed20879a0a3c59c7a91f8248591
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-01-26 20:16:25 +0800

    simplify sbin/manti-table-jobs.sql
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- sbin/manti-table-jobs.sql
+++ sbin/manti-table-jobs.sql
@@ -16,59 +16,16 @@
 
 CREATE TABLE jobs(
 	id		bigint,
-	submit_id	bigint,
-	submit_time	bigint,
-	submit_date	string,
+	submit_id	bigint indexed,
+	submit_time	bigint indexed,
+	boot_time	bigint indexed,
+	finish_time	bigint indexed,
+	boot_seconds	int indexed,
+	run_seconds	int indexed,
 
-	my_account	string,
-	my_name		string,
+	j		json,
+	full_text_kv	text indexed,
 
-	suite		string,
-	category	string,
-
-	os		string,
-	os_version	string,
-	os_arch		string,
-
-	osv		string,
-	os_mount	string,
-	rootfs		string,
-
-	pp_params_md5	string,
-	all_params_md5	string,
-
-	queue		string,
-
-	lab		string,
-	tbox_group	string,
-
-	pp		json,
-	ss		json,
-	hw		json,
-
-	boot_time	bigint engine='rowwise',
-	running_time	bigint engine='rowwise',
-	finish_time	bigint engine='rowwise',
-	time		bigint engine='rowwise',
-
-	boot_seconds	int engine='rowwise',
-	run_seconds	int engine='rowwise',
-
-	jj		json engine='rowwise',
-
-	testbox		string engine='rowwise',
-
-	job_stage	string engine='rowwise',
-	job_health	string engine='rowwise',
-
-	mutable_vars	string stored engine='rowwise',
-
-	errid		string,
-	stats		string stored,
-
-	other_data	string stored,
-
-	full_text_words text indexed,
 ) engine='columnar' charset_table='U+0021..U+007E';
 
 desc jobs;
