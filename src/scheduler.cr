9d0efc75846f546ff98cef85c99a3131e56ac9fb
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-02-06 14:39:50 +0800

    rename scheduler/scheduler.cr to scheduler/kemal.cr
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- src/scheduler.cr
+++ src/scheduler.cr
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
 
-require "./scheduler/scheduler"
 require "./scheduler/constants.cr"
+require "./scheduler/kemal.cr"
 require "./lib/json_logger"
 require "./lib/do_local_pack"
 require "./lib/create_secrets_yaml"
