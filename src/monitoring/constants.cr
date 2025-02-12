e71d9b408eee58af41c3a71a162964f5ddea1b52
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-02-12 19:20:43 +0800

    remove monitoring service
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- src/monitoring/constants.cr
+++
@@ -1,4 +0,0 @@
-MQ_HOST = (ENV.has_key?("MQ_HOST") ? ENV["MQ_HOST"] : "172.17.0.1")
-MQ_PORT = (ENV.has_key?("MQ_PORT") ? ENV["MQ_PORT"] : 5672).to_i32
-
-MONITOR_PORT = (ENV.has_key?("MONITOR_PORT") ? ENV["MONITOR_PORT"] : 20001).to_i32
