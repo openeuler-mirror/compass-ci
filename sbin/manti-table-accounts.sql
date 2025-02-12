49191fcb347c4218df9e6942f26d6fa9595cea36
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-02-12 20:13:56 +0800

    manticore: add accounts table
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

---
+++ sbin/manti-table-accounts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE hosts(
+	id			bigint,
+	gitee_id                string,   
+	my_account              string,   
+	my_commit_url           string,   
+	my_email                string,   
+	my_login_name           string,   
+	my_name                 string,   
+	my_token                string,   
+	weight                  int,   
+) charset_table='U+0021..U+007E'
