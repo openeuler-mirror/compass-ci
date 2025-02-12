09f34ea40996f9ce36e24529cb6baefaf678e460
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-02-12 17:13:55 +0800

    remove watch-jobs service
    Let's use the integrated wait_on mechanism.
    
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- src/watch-jobs/constants.cr
+++
@@ -1,8 +0,0 @@
-WATCH_STATE = {
-  "job_health" => {
-    "bad" => %w[failed incomplete abnormal timeout crash OOM
-                load_disk_fail error_mount wget_kernel_fail
-                wget_initrd_fail initrd_broken kexec_fail],
-    "good" => ["success"]
-  }
-}
