775105605989b4d4c1b5691faefef62127a75436
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-02-19 19:47:44 +0800

    docker entrypoint: fix cp warning
    cp: cannot stat '/lkp/lkp/src/rootfs/addon/root/.': No such file or directory
    
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- providers/docker/bin/entrypoint.sh
+++ providers/docker/bin/entrypoint.sh
@@ -2,7 +2,13 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
 
-mkdir -p /root/.ssh
-cp -r /lkp/lkp/src/rootfs/addon/root/. /root
-cp -r /lkp/lkp/src/rootfs/addon/usr/* /usr
+test -d /lkp/lkp/src/rootfs/addon/root && {
+	mkdir -p /root/.ssh
+	cp -r /lkp/lkp/src/rootfs/addon/root/. /root
+}
+
+test -d /lkp/lkp/src/rootfs/addon/usr && {
+	cp -r /lkp/lkp/src/rootfs/addon/usr/* /usr
+}
+
 sh /lkp/lkp/src/rootfs/addon/etc/init.d/lkp-bootstrap
