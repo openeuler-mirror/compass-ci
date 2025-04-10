f4be7f0fdbcf1a1060ef838d762ae8e6a89df6d5
Wu Fengguang <wfg@mail.ustc.edu.cn>
2025-04-10 16:59:05 +0800

    docker: fix silent cpio error in entrypoint.sh
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- providers/docker/bin/entrypoint.sh
+++ providers/docker/bin/entrypoint.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
 
 # Some docker images dont have cpio command
 [ "${PATH%%:/opt/busybox:*}" = "$PATH" ] && export PATH="$PATH:/opt/busybox"
+
+# catch cpio failures
+set -o pipefail
 
 # cgz files containing /usr/* files have to be unpacked inside container
 (
@@ -11,7 +14,7 @@
     for file in $(ls -1tr /lkp/cpio-for-guest/*.cgz)
     do
         test -e "$file" || continue
-        gzip -dc "$file" | cpio -idu >/dev/null 2>&1
+        gzip -dc "$file" | cpio -idu >/dev/null
     done
 )
 
