8bc0097e9029f6360b377c16a99d5f3988dca9de
wq <2332054245@qq.com>
2025-07-03 16:23:36 +0800

    修复cci return和cci lkp-renew命令

--- src/update_job_parameter.cr
+++ src/update_job_parameter.cr
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
+require "time"
 
 class Sched
 
@@ -12,6 +13,7 @@ class Sched
 
   # Update job attributes from a hash of parameters
   def update_job_from_hash(params : Hash(String, String)) : Result
+    results = [] of String
     job_id = params["job_id"]?
     return Result.error(HTTP::Status::BAD_REQUEST, "Error: Missing job_id") unless job_id
 
@@ -40,6 +42,7 @@ class Sched
         update_milestones(job, value)
       when "renew_seconds"
         result = renew_job(job, value)
+        results << result.message
         return result unless result.success
       end
     end
@@ -48,7 +51,7 @@ class Sched
     send_job_event(job.id64, params.to_json)
     report_workflow_job_event(job.id64, job)
 
-    Result.success("")
+    Result.success(results.join("\n"))
   end
 
   # Helper method to update job state or stage
@@ -80,7 +83,10 @@ class Sched
     end
 
     job.renew_addtime(value.to_i32)
-    Result.success("Renewed job for #{value} seconds")
+    borrow_seconds = job.hash_int32["borrow_seconds"]? || 0
+    dispatch_time = Time.parse_iso8601(job.hash_plain["dispatch_time"]?)
+    new_time = dispatch_time + borrow_seconds.seconds
+    Result.success(new_time.to_s("%Y-%m-%dT%H:%M:%S%:z"))
   end
 
 end
